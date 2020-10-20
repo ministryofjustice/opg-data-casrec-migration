0bf15a2d809333c7145680000c9328515b672181
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-10-20 16:01:34 +0100

    first few simple tests

---
+++ etl2/app/tests/transformations/conftest.py
@@ -0,0 +1,38 @@
+import pytest
+
+from logger import custom_logger
+from transformations import transformations_from_mapping
+
+logger = custom_logger(name="transformation_test")
+
+
+@pytest.fixture()
+def mock_standard_transformations(monkeypatch):
+    def mock_squash_columns(original_cols, final_cols, df):
+        logger.info("mock squash_columns")
+        return df
+
+    def mock_convert_to_bool(original_cols, final_cols, df):
+        logger.info("mock convert_to_bool")
+        return df
+
+    def mock_date_format_standard(original_cols, final_cols, df):
+        logger.info("mock date_format_standard")
+        return df
+
+    def mock_unique_number(final_cols, df):
+        logger.info("mock unique_number")
+        return df
+
+    monkeypatch.setattr(
+        transformations_from_mapping, "squash_columns", mock_squash_columns
+    )
+    monkeypatch.setattr(
+        transformations_from_mapping, "convert_to_bool", mock_convert_to_bool
+    )
+    monkeypatch.setattr(
+        transformations_from_mapping, "date_format_standard", mock_date_format_standard
+    )
+    monkeypatch.setattr(
+        transformations_from_mapping, "unique_number", mock_unique_number
+    )
