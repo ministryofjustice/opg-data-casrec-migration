0a3ee1469e9fd8a48ea56c6845af7d9f6987ec29
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-01-06 09:03:08 +0000

    made the filename work better

--- migration_steps/shared/decorators.py
+++ migration_steps/shared/decorators.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import sys
 import time
 
+
 log = logging.getLogger("root")
 environment = os.environ.get("ENVIRONMENT")
 
@@ -13,11 +14,12 @@ environment = os.environ.get("ENVIRONMENT")
 def timer(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+
         t = time.process_time()
 
         function_path = Path(sys.modules[func.__module__].__file__).parts
-        transformation_step = function_path[function_path.index("migration_steps") + 1]
         function_name = func.__name__
+        filename = function_path[-1]
 
         try:
             return func(*args, **kwargs)
@@ -26,9 +28,8 @@ def timer(func):
             time_to_run = round(time.process_time() - t, 2)
 
             log.debug(
-                f"Function '{function_name}' in step "
-                f"'{transformation_step}' ran in "
-                f"{time_to_run} secs"
+                f"Function '{function_name}' from '{filename}' ran in "
+                f"{time_to_run} secs "
             )
 
     return wrapper
