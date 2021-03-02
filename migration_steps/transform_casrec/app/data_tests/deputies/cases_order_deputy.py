eb44bb580f91dba24d0aad78d91e10ffb5ef496b
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-03-02 16:32:11 +0000

    conditional lookup test

--- migration_steps/transform_casrec/app/data_tests/deputies/cases_order_deputy.py
+++ migration_steps/transform_casrec/app/data_tests/deputies/cases_order_deputy.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+from pandas.core.common import flatten
 from pytest_cases import case
 
 
@@ -20,6 +21,54 @@ def case_order_deputies_lookups(test_config):
     config = test_config
 
     source_columns = [f'"{y}"' for x in lookup_fields.values() for y in x]
+    transformed_columns = [f'"{x}"' for x in lookup_fields.keys()]
+
+    source_query = f"""
+        SELECT
+            "{merge_columns['source']}",
+            {', '.join(source_columns)}
+        FROM {config.schemas['pre_transform']}.{source_table}
+
+    """
+
+    transformed_query = f"""
+        SELECT
+            {merge_columns['transformed']},
+            {', '.join(transformed_columns)}
+        FROM {config.schemas['post_transform']}.{destination_table}
+        WHERE casrec_mapping_file_name = '{module_name}'
+    """
+
+    return (lookup_fields, merge_columns, source_query, transformed_query, module_name)
+
+
+@case(tags="conditional_lookup")
+def case_order_deputies_con_lookups(test_config):
+
+    lookup_fields = {
+        "statuschangedate": {
+            "cols": {
+                "result": "Disch Death",
+                "reference": "Stat",
+            },
+            "lookup_def": "discharge_lookup",
+        }
+    }
+    merge_columns = {"source": "Deputy No", "transformed": "c_deputy_no"}
+
+    config = test_config
+
+    source_columns = [
+        f'"{x}"'
+        for x in flatten(
+            [
+                list(z.values())
+                for x in lookup_fields.values()
+                for y, z in x.items()
+                if y == "cols"
+            ]
+        )
+    ]
     transformed_columns = [f'"{x}"' for x in lookup_fields.keys()]
 
     source_query = f"""
---
+++ migration_steps/transform_casrec/app/data_tests/test_conditional_lookup.py
@@ -0,0 +1,113 @@
+import re
+
+from pytest_cases import parametrize_with_cases
+
+from data_tests.conftest import (
+    list_of_test_cases,
+    add_to_tested_list,
+)
+from data_tests.helpers import (
+    get_data_from_query,
+    get_merge_col_data_as_list,
+    merge_source_and_transformed_df,
+)
+import logging
+
+
+from helpers import get_lookup_dict
+from datetime import datetime
+
+log = logging.getLogger("root")
+
+
+@parametrize_with_cases(
+    (
+        "lookup_fields",
+        "merge_columns",
+        "source_query",
+        "transformed_query",
+        "module_name",
+    ),
+    cases=list_of_test_cases,
+    has_tag="conditional_lookup",
+)
+def test_map_conditional_lookup_tables(
+    test_config,
+    lookup_fields,
+    merge_columns,
+    source_query,
+    transformed_query,
+    module_name,
+):
+    print(f"module_name: {module_name}")
+
+    add_to_tested_list(
+        module_name=module_name,
+        tested_fields=[x for x in lookup_fields.keys()]
+        + [merge_columns["transformed"]],
+    )
+
+    config = test_config
+
+    source_sample_df = get_data_from_query(
+        query=source_query, config=config, sort_col=merge_columns["source"], sample=True
+    )
+
+    assert source_sample_df.shape[0] > 0
+
+    sample_caserefs = get_merge_col_data_as_list(
+        df=source_sample_df, column_name=merge_columns["source"]
+    )
+
+    transformed_df = get_data_from_query(
+        query=transformed_query,
+        config=config,
+        sort_col=merge_columns["transformed"],
+        sample=False,
+    )
+
+    assert transformed_df.shape[0] > 0
+
+    transformed_sample_df = transformed_df[
+        transformed_df[merge_columns["transformed"]].isin(sample_caserefs)
+    ]
+
+    result_df = merge_source_and_transformed_df(
+        source_df=source_sample_df,
+        transformed_df=transformed_sample_df,
+        merge_columns=merge_columns,
+    )
+
+    log.debug(
+        f"Checking {result_df.shape[0]} rows of data ({config.SAMPLE_PERCENTAGE}%) from table: {module_name} "
+    )
+
+    assert result_df.shape[0] > 0
+    for k, v in lookup_fields.items():
+        lookup_filename = lookup_fields[k]["lookup_def"]
+
+        for i, j in v.items():
+            result_col = lookup_fields[k]["cols"]["result"]
+
+            reference_col = lookup_fields[k]["cols"]["reference"]
+
+            lookup_dict = get_lookup_dict(file_name=lookup_filename.lower())
+
+            result_df["mapped"] = result_df[reference_col].map(lookup_dict)
+
+            result_df = result_df.loc[result_df["mapped"].notnull()]
+
+            result_df[result_col] = result_df[result_col].apply(lambda x: f"{x}")
+            result_df[k] = result_df[k].apply(lambda x: f"{x}")
+
+            assert result_df[result_col].fillna("").equals(result_df[k].fillna(""))
+
+            match = result_df[result_col].fillna("").equals(result_df[k].fillna(""))
+
+            print(
+                f"checking {k} == {result_col}...."
+                f""
+                f" {'OK' if match is True else 'oh no'} ",
+            )
+
+            assert match is True
