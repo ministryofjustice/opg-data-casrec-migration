a397e119aa3ad87c70004e75677af086df39d739
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-03-03 13:15:42 +0000

    basic bonds data

--- migration_steps/transform_casrec/app/utilities/standard_transformations.py
+++ migration_steps/transform_casrec/app/utilities/standard_transformations.py
@@ -74,3 +74,9 @@ def capitalise(original_col: str, result_col: str, df: pd.DataFrame) -> pd.DataF
     df = df.drop(columns=[original_col])
 
     return df
+
+
+def money(original_col: str, result_col: str, df: pd.DataFrame) -> pd.DataFrame:
+    df[result_col] = df[original_col].apply(lambda x: x)
+    df = df.drop(columns=[original_col])
+    return df
