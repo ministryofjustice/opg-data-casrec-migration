390d788a96308237202d33d266ee3931a34c3776
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-03-23 16:55:26 +0000

    consolidate requirements and move as many bits as poss to new logging

--- migration_steps/load_to_sirius/app/app.py
+++ migration_steps/load_to_sirius/app/app.py
@@ -11,7 +11,7 @@ current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, str(current_path) + "/../../../shared")
 
 from audit import run_audit
-import logging
+import logging.config
 import time
 import click
 from sqlalchemy import create_engine
@@ -19,7 +19,6 @@ import custom_logger
 from helpers import log_title
 from progress import update_progress
 import table_helpers
-
 from dotenv import load_dotenv
 
 
@@ -33,12 +32,12 @@ import helpers
 
 config = helpers.get_config(env=environment)
 
+
 # logging
-# custom_logger.custom_log_level(levels=config.custom_log_levels)
-config.custom_log_level()
-verbosity_levels = config.verbosity_levels
 log = logging.getLogger("root")
-log.addHandler(custom_logger.MyHandler())
+# custom_logger.setup_logging(env="environment")
+custom_logger.setup_logging(env=environment)
+
 
 # database
 db_config = {
@@ -54,17 +53,8 @@ completed_tables = []
 
 
 @click.command()
-@click.option("-v", "--verbose", count=True)
 @click.option("-a", "--audit", count=False)
-def main(verbose, audit):
-    try:
-        log.setLevel(verbosity_levels[verbose])
-        log.info(f"{verbosity_levels[verbose]} logging enabled")
-    except KeyError:
-        log.setLevel("INFO")
-        log.info(f"{verbose} is not a valid verbosity level")
-        log.info(f"INFO logging enabled")
-
+def main(audit):
     log.info(log_title(message="Load to Target Step: AKA do the migration already"))
     log.info(
         log_title(
