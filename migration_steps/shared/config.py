f30ac547a47db51ce22b2fd148898f8c268fff78
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-04-26 12:15:03 +0100

    fixing things and making it a bit faster

--- migration_steps/shared/config.py
+++ migration_steps/shared/config.py
@@ -67,8 +67,8 @@ class BaseConfig:
     ENABLED_ENTITIES = {
         "clients": True,
         "cases": True,
-        "bonds": True,
-        "supervision_level": True,
+        "bonds": False,
+        "supervision_level": False,
         "deputies": True,
         "events": False,
         "finance": False,
