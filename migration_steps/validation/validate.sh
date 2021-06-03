a383280989278ae4149260ffab0b3e05af52d117
james.warren <james.warren@digital.justice.gov.uk>
2021-06-03 17:09:06 +0100

    fix conf

--- migration_steps/validation/validate.sh
+++ migration_steps/validation/validate.sh
@@ -7,8 +7,8 @@ then
   python3 "${DIR}/api_test_data_s3_upload/app/app.py"
 fi
 
-#python3 "${DIR}/validate_db/app/app.py" "$@"
-#python3 "${DIR}/post_migration_tests/app/app.py"
+python3 "${DIR}/validate_db/app/app.py" "$@"
+python3 "${DIR}/post_migration_tests/app/app.py"
 
 if [ "${RUN_API_TESTS}" == "True" ]
 then
