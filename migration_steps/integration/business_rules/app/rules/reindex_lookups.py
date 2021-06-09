f6cbd2725c77f414f324c3c4362285713b4af2a3
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-06-09 15:08:40 +0100

    defs with new container

---
+++ migration_steps/integration/business_rules/app/rules/reindex_lookups.py
@@ -0,0 +1,9 @@
+parent_table = "bonds"
+parent_match_col = "bond_provider_id"
+lookup_table = "bond_providers"
+lookup_result_col = "id"
+lookup_match_col = "uid"
+
+
+def reindex_lookup():
+    pass
