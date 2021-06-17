e92dad04aa943aa36119a9e45467d3a9b247fbc5
jmackown <jmackown@users.noreply.github.com>
2021-06-17 10:34:17 +0100

    In 746 checker (#235)
    * check lookups & sync staging
    
    * do it in the prepare step
    
    * use new prepare container
    
    * updated defs
    
    * fix a few mapping things
    
    * quicky preprod
    
    * Revert "quicky preprod"
    
    This reverts commit 0767c3fe1e3aafc7fe69280c7a84cac2b447a7bd.

---
+++ migration_steps/prepare/create_stage_schema/app/lookups/check_lookups_in_mapping.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import helpers
+import pandas as pd
+from lookups.utilities import get_lookups_to_sync
+
+log = logging.getLogger("root")
+environment = os.environ.get("ENVIRONMENT")
+
+
+config = helpers.get_config(env=environment)
+
+
+def get_sirius_data(db_config, table):
+
+    sirius_statement = f"""select * from {db_config['sirius_schema']}.{table};"""
+
+    lookup_data = pd.read_sql_query(
+        sql=sirius_statement, con=db_config["sirius_db_connection_string"]
+    )
+
+    lookup_data = lookup_data.fillna("")
+
+    return lookup_data
+
+
+def check_single_lookup(db_config, table, lookup_file_name):
+    log.info(f"Checking mapping values for lookup def {lookup_file_name}")
+
+    lookup_dict = helpers.get_lookup_dict(file_name=lookup_file_name)
+    unique_vals = list(set([x for x in lookup_dict.values()]))
+
+    sirius_data_df = get_sirius_data(
+        db_config=db_config,
+        table=table,
+    )
+
+    sirius_values = sirius_data_df["id"].tolist()
+
+    return all(x in sirius_values for x in unique_vals)
+
+
+def check_lookups(db_config):
+    lookups_to_check = get_lookups_to_sync()
+
+    for lookup, details in lookups_to_check.items():
+        for field in details:
+            for field_to_sync, lookup_table_def in field.items():
+                lookup_file_name = list(lookup_table_def.keys())[0]
+                sirius_lookup_table = list(lookup_table_def.values())[0]
+                is_lookup_ok = check_single_lookup(
+                    db_config=db_config,
+                    table=sirius_lookup_table,
+                    lookup_file_name=lookup_file_name,
+                )
+
+                if is_lookup_ok:
+                    log.info(f"All lookup values in the {lookup_file_name} are OK")
+                else:
+                    log.error(
+                        f"Lookup values in {lookup_file_name} do not match the destination, exiting"
+                    )
+                    os._exit(1)
---
+++ migration_steps/prepare/create_stage_schema/app/lookups/utilities.py
@@ -0,0 +1,44 @@
+import logging
+import os
+
+import helpers
+from helpers import get_mapping_dict, list_all_mapping_files
+
+
+log = logging.getLogger("root")
+environment = os.environ.get("ENVIRONMENT")
+
+
+config = helpers.get_config(env=environment)
+
+
+def get_lookups_to_sync():
+    mapping_files = list_all_mapping_files()
+
+    lookups_to_sync = {}
+    for file in mapping_files:
+        file_name = file[:-5]
+        mapping_dict = get_mapping_dict(file_name=file_name, stage_name="prepare")
+
+        for field, details in mapping_dict.items():
+            if "sync_lookup" in details["sync"]:
+                lookup_def = get_mapping_dict(
+                    file_name=file_name, stage_name="transform_casrec"
+                )[field]["lookup_table"]
+                sirius_table = get_mapping_dict(
+                    file_name=file_name, stage_name="sirius_details"
+                )[field]["table_name"]
+                lookup_table = get_mapping_dict(
+                    file_name=file_name, stage_name="sirius_details"
+                )[field]["fk_parents"].split(":")[0]
+
+                if sirius_table in lookups_to_sync:
+                    lookups_to_sync[sirius_table].append(
+                        {field: {lookup_def: lookup_table}}
+                    )
+                else:
+                    lookups_to_sync[sirius_table] = [
+                        {field: {lookup_def: lookup_table}}
+                    ]
+
+    return lookups_to_sync
