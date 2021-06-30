6d430d2fac6949f4b8dfd054eebdc6a8bf4f62f2
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-06-30 15:24:48 +0100

    get it to sirius!

--- migration_steps/transform_casrec/transform/app/entities/reporting/annual_report_logs.py
+++ migration_steps/transform_casrec/transform/app/entities/reporting/annual_report_logs.py
@@ -2,6 +2,7 @@ from custom_errors import EmptyDataFrame
 from utilities.basic_data_table import get_basic_data_table
 import logging
 import os
+import pandas as pd
 
 from helpers import get_mapping_dict
 
@@ -9,8 +10,8 @@ log = logging.getLogger("root")
 
 definition = {
     "source_table_name": "account",
-    "source_table_additional_columns": [],
-    "destination_table_name": "annual_report_log",
+    "source_table_additional_columns": ["Case"],
+    "destination_table_name": "annual_report_logs",
 }
 
 mapping_file_name = "annual_report_logs_mapping"
@@ -21,6 +22,14 @@ def insert_annual_report_logs(db_config, target_db):
     chunk_size = db_config["chunk_size"]
     offset = 0
     chunk_no = 1
+
+    persons_query = (
+        f'select "id", "caserecnumber" from {db_config["target_schema"]}.persons '
+        f"where \"type\" = 'actor_client';"
+    )
+    persons_df = pd.read_sql_query(persons_query, db_config["db_connection_string"])
+
+    persons_df = persons_df[["id", "caserecnumber"]]
 
     sirius_details = get_mapping_dict(
         file_name=mapping_file_name,
@@ -37,12 +46,30 @@ def insert_annual_report_logs(db_config, target_db):
                 chunk_details={"chunk_size": chunk_size, "offset": offset},
             )
 
-            target_db.insert_data(
-                table_name=definition["destination_table_name"],
-                df=annual_report_log_df,
-                sirius_details=sirius_details,
-                chunk_no=chunk_no,
+            annual_report_log_joined_df = annual_report_log_df.merge(
+                persons_df,
+                how="left",
+                left_on="c_case",
+                right_on="caserecnumber",
             )
+
+            annual_report_log_joined_df["client_id"] = annual_report_log_joined_df[
+                "id_y"
+            ]
+            annual_report_log_joined_df = annual_report_log_joined_df.drop(
+                columns=["id_y"]
+            )
+            annual_report_log_joined_df = annual_report_log_joined_df.rename(
+                columns={"id_x": "id"}
+            )
+
+            if len(annual_report_log_joined_df) > 0:
+
+                target_db.insert_data(
+                    table_name=definition["destination_table_name"],
+                    df=annual_report_log_joined_df,
+                    sirius_details=sirius_details,
+                )
 
             offset += chunk_size
             chunk_no += 1
