a72018719bd1039e571896560d27f60152cd512b
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-07-02 16:38:35 +0100

    exclude it from the loop through def files

--- migration_steps/shared/helpers.py
+++ migration_steps/shared/helpers.py
@@ -197,24 +197,27 @@ def get_all_mapped_fields(
     for json_file in os.listdir(file_path):
         json_file_path = os.path.join(file_path, json_file)
         if os.path.isfile(json_file_path):
-            with open(json_file_path, "r") as definition_json:
-                def_dict = json.load(definition_json)
+            if "table_definition" in json_file_path:
+                pass
+            else:
+                with open(json_file_path, "r") as definition_json:
+                    def_dict = json.load(definition_json)
 
-                key_name = json_file.replace("_mapping.json", "")
-                if include_keys:
-                    all_mapping_dicts[key_name] = [
-                        k
-                        for k, v in def_dict.items()
-                        if v["mapping_status"]["is_complete"] is complete
-                    ]
-                else:
-                    all_mapping_dicts[key_name] = [
-                        k
-                        for k, v in def_dict.items()
-                        if v["mapping_status"]["is_complete"] is complete
-                        and v["sirius_details"]["is_pk"] is not True
-                        and len(v["sirius_details"]["fk_parents"]) == 0
-                    ]
+                    key_name = json_file.replace("_mapping.json", "")
+                    if include_keys:
+                        all_mapping_dicts[key_name] = [
+                            k
+                            for k, v in def_dict.items()
+                            if v["mapping_status"]["is_complete"] is complete
+                        ]
+                    else:
+                        all_mapping_dicts[key_name] = [
+                            k
+                            for k, v in def_dict.items()
+                            if v["mapping_status"]["is_complete"] is complete
+                            and v["sirius_details"]["is_pk"] is not True
+                            and len(v["sirius_details"]["fk_parents"]) == 0
+                        ]
 
     return all_mapping_dicts
 
