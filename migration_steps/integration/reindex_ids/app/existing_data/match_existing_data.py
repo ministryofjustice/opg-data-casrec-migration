03cd4211e2e12cb01d56e3026e26df1f35824ec5
jmackown <jmackown@users.noreply.github.com>
2021-08-04 12:11:16 +0100

    IN-845 Harder Better Faster Stronger (#289)
    * change the matching to use a temp table instead of huge dictionaries!
    
    * jimstructions

--- migration_steps/integration/reindex_ids/app/existing_data/match_existing_data.py
+++ migration_steps/integration/reindex_ids/app/existing_data/match_existing_data.py
@@ -3,7 +3,8 @@ import psycopg2
 import os
 import sys
 from pathlib import Path
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, str(current_path) + "/../../../../shared")
@@ -22,65 +23,40 @@ def match_existing_data(db_config, table_details):
 
     tables_to_match = get_tables_to_match(table_details=table_details)
 
-    match_indicator = "UPDATE"
-
     for table, details in tables_to_match.items():
+
         existing_data_details = details["existing_data"]
+
+        create_temp_table(
+            db_config=db_config, table_name=table, table_details=existing_data_details
+        )
+        get_existing_data(
+            db_config=db_config, table_name=table, table_details=existing_data_details
+        )
+        update_casrec_rows(
+            db_config=db_config, table_name=table, table_details=existing_data_details
+        )
+
+        if unmatched_check(
+            db_config=db_config, table_name=table, table_details=existing_data_details
+        ):
+            log.debug(f"All rows matched for table {table}")
+        else:
+            log.error(f"Not all rows matched for table {table}")
+            # os._exit(1)
 
         tables_with_fk_links_to_this_table = fks(
             table_details=table_details, parent_table=table
         )
-        updates = []
 
-        matching_data = get_matching_data(
-            db_config=db_config,
-            table_name=table,
-            existing_data_details=existing_data_details,
-        )
-
-        for match_col, match_ids in matching_data.items():
-
-            update_query = f"""
-                UPDATE {db_config['target_schema']}.{table}
-                SET id = {match_ids['existing_id']},
-                method = '{match_indicator}',
-                pk_source = 'sirius'
-                WHERE id = {match_ids['migration_id']}
-                AND {existing_data_details['match_field']} = '{match_col}';
-            """
-
-            updates.append(update_query)
-
-        connection_string = db_config["db_connection_string"]
-        conn = psycopg2.connect(connection_string)
-        cursor = conn.cursor()
-
-        if len(updates) == 0:
-            log.error("Nothing to update, but there should be...")
-            # TODO - this should kill the process, awaiting test data. Then get rid of the else below
-            # os._exit(1)
-        else:
-            try:
-                all_updates = " ".join(updates)
-                cursor.execute(all_updates)
-            except Exception as e:
-                log.error(
-                    f"There was an error updating the matched data: {e}",
-                    extra=format_error_message(e=e),
-                )
-                os._exit(1)
-
-            cursor.close()
-            conn.commit()
-
-            try:
-                update_fks(
-                    db_config=db_config,
-                    table_details=tables_with_fk_links_to_this_table,
-                    match=True,
-                )
-            except Exception as e:
-                print(f"e: {e}")
+        try:
+            update_fks(
+                db_config=db_config,
+                table_details=tables_with_fk_links_to_this_table,
+                match=True,
+            )
+        except Exception as e:
+            log.error(f"{e}")
 
 
 def fks(table_details, parent_table):
@@ -95,58 +71,146 @@ def fks(table_details, parent_table):
     return tables_to_reindex_fks
 
 
-def get_matching_data(db_config, table_name, existing_data_details):
-
-    existing_records = get_records_to_match(
-        schema=db_config["sirius_schema"],
-        connection_string=db_config["sirius_db_connection_string"],
-        table_name=table_name,
-        table_details=existing_data_details,
-    )
-    new_records = get_records_to_match(
-        schema=db_config["source_schema"],
-        connection_string=db_config["db_connection_string"],
-        table_name=table_name,
-        table_details=existing_data_details,
-    )
-
-    matching_records = {}
-    for match_field, id in new_records.items():
-        if match_field in existing_records:
-            matching_records[match_field] = {
-                "migration_id": id,
-                "existing_id": existing_records.get(match_field),
-            }
-
-    log.debug(f"Found {len(matching_records)} matching records")
-
-    if len(matching_records) < len(new_records):
-        log.error(
-            f"All records should match, we're missing {len(new_records) - len(matching_records)} matches"
-        )
-
-    return matching_records
-
-
-def get_records_to_match(schema, connection_string, table_name, table_details):
+def create_temp_table(db_config, table_name, table_details):
 
     query = f"""
-        select id, "{table_details['match_field']}" from {schema}.{table_name}
+        CREATE TABLE IF NOT EXISTS {db_config['target_schema']}.existing_{table_name}
+        (existing_id integer,
+        {table_details['match_field']} text);
+    """
+
+    try:
+        conn = psycopg2.connect(db_config["db_connection_string"])
+        cursor = conn.cursor()
+        cursor.execute(query)
+        cursor.close()
+        conn.commit()
+    except Exception as e:
+        log.error(e)
+
+
+def get_existing_data(db_config, table_name, table_details):
+    target_db_engine = create_engine(db_config["db_connection_string"])
+    chunk_size = db_config["chunk_size"]
+    offset = 0
+
+    while True:
+
+        query = f"""
+            select id, "{table_details['match_field']}" from {db_config['sirius_schema']}.{table_name}
+        """
+        if "conditions" in table_details:
+            conditions = format_conditions(conditions=table_details.get("conditions"))
+            query += conditions
+        query += f" ORDER BY id LIMIT {chunk_size} OFFSET {offset};"
+
+        existing_data = pd.read_sql_query(
+            sql=query, con=db_config["sirius_db_connection_string"]
+        )
+
+        insert_statement = create_insert_into_temp_table(
+            db_config=db_config,
+            df=existing_data,
+            table_name=table_name,
+            table_details=table_details,
+        )
+
+        try:
+            target_db_engine.execute(insert_statement)
+        except Exception as e:
+            log.error(e)
+            log.error(
+                f"There was an error inserting {len(existing_data)} rows "
+                f"into {db_config['target_schema']}.{table_name}",
+                extra={
+                    "table_name": table_name,
+                    "size": len(existing_data),
+                    "action": "insert",
+                    "error": format_error_message(e=e),
+                },
+            )
+            os._exit(1)
+
+        offset += chunk_size
+        log.debug(f"doing offset {offset} for table {table_name}")
+        if len(existing_data) < chunk_size:
+            break
+
+
+def create_insert_into_temp_table(db_config, df, table_name, table_details):
+
+    statement = f"""
+        INSERT INTO {db_config['target_schema']}.existing_{table_name} (existing_id, {table_details['match_field']}) VALUES
+    """
+    for i, row in enumerate(df.values.tolist()):
+        single_row = f"({row[0]}, '{row[1]}')"
+        if i + 1 < len(df):
+            single_row += ", "
+        else:
+            single_row += "; "
+
+        statement += single_row
+
+    return statement
+
+
+def update_casrec_rows(db_config, table_name, table_details):
+    target_db_engine = create_engine(db_config["db_connection_string"])
+
+    statement = f"""
+        UPDATE {db_config['target_schema']}.{table_name}
+        SET id = existing_id, method='UPDATE'
+        FROM {db_config['target_schema']}.existing_{table_name}
+        WHERE {table_name}.{table_details['match_field']} = existing_{table_name}.{table_details['match_field']};
+
+    """
+
+    try:
+        target_db_engine.execute(statement)
+    except Exception as e:
+        log.error(
+            f"There was an error updating existing data from existing_{table_name} to {table_name} using {table_details['match_field']}",
+            extra={
+                "table_name": table_name,
+                "error": format_error_message(e=e),
+            },
+        )
+        os._exit(1)
+
+
+def get_unmatched_rows(db_config, table_name, table_details):
+    target_db_engine = create_engine(db_config["db_connection_string"])
+
+    query = f"""
+        SELECT count(*), method FROM {db_config['target_schema']}.{table_name}
     """
     if "conditions" in table_details:
         conditions = format_conditions(conditions=table_details.get("conditions"))
         query += conditions
 
-    conn = psycopg2.connect(connection_string)
-    cursor = conn.cursor()
+    query += f""" GROUP BY method;"""
 
-    records = {}
     try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        records = {x[1]: x[0] for x in result}
+        result = target_db_engine.execute(query).fetchall()
+        return result
     except Exception as e:
         log.error(e)
-    cursor.close()
-    conn.commit()
-    return records
+
+
+def unmatched_check(db_config, table_name, table_details):
+
+    unmatched_rows = get_unmatched_rows(
+        db_config=db_config, table_name=table_name, table_details=table_details
+    )
+
+    stats = {}
+    for x in unmatched_rows:
+        stats[x[1]] = x[0]
+    try:
+        if stats["INSERT"] > 0:
+            log.error(f"matched {stats['UPDATE']}, unmatched {stats['INSERT']}")
+            return False
+        else:
+            return True
+    except KeyError:
+        return True
