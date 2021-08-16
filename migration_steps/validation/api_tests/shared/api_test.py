23608e64dd37490f74b96448effe82e0a2afe63c
james.warren <james.warren@digital.justice.gov.uk>
2021-08-16 15:03:42 +0100

    IN-876 add new preqa env to our environment

--- migration_steps/validation/api_tests/shared/api_test.py
+++ migration_steps/validation/api_tests/shared/api_test.py
@@ -613,10 +613,10 @@ class ApiTests:
                     formatted_api_response = self.get_formatted_api_response(
                         entity_ids, endpoint, headers_to_check, row, entity_ref
                     )
-            # Loop through and check expected results against actual for each field
-            self.assert_on_fields(
-                headers_to_check, formatted_api_response, row, entity_ref
-            )
+                # Loop through and check expected results against actual for each field
+                self.assert_on_fields(
+                    headers_to_check, formatted_api_response, row, entity_ref
+                )
             self.api_log(f"Ran happy path tests against {count} cases in {self.csv}")
         else:
             self.api_log(
