38e6fc5f9c489d414730491578335b19ae9dcd54
james.warren <james.warren@digital.justice.gov.uk>
2021-09-08 15:50:32 +0100

    fix some minor issues

--- migration_steps/load_to_sirius/delete_existing_data/sql/alternative_statements.sql
+++ migration_steps/load_to_sirius/delete_existing_data/sql/alternative_statements.sql
@@ -1,4 +1,4 @@
-delete from events where source_person_id in (select id from persons where clientsource = 'SKELETON');
-delete from events where owning_donor_id in (select id from persons where clientsource = 'SKELETON');
-delete from document_secondaryrecipient where person_id in (select id from persons where clientsource = 'SKELETON');
-delete from documents where correspondent_id in (select id from persons where clientsource = 'SKELETON');
+-- LIST OF EXTRA STATEMENTS THAT MAY BE NEEDED BUT ARE NOT CURRENTLY USED
+--delete from events where source_person_id in (select id from persons where clientsource = 'SKELETON');
+--delete from events where owning_donor_id in (select id from persons where clientsource = 'SKELETON');
+--delete from persons where fee_payer_id in (select id from persons where clientsource = 'SKELETON');
