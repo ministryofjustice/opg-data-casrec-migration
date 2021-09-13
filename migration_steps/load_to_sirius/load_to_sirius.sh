c6c5eb8f2c7f0e7e88115e53dc05fb947abc8a21
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-09-13 10:45:25 +0100

    Move from load to prepare

--- migration_steps/load_to_sirius/load_to_sirius.sh
+++ migration_steps/load_to_sirius/load_to_sirius.sh
@@ -8,7 +8,6 @@ if [ "${ENVIRONMENT}" == "local" ] \
   || [ "${ENVIRONMENT}" == "qa" ]
   then
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-  python3 "${DIR}/delete_existing_data/app.py"
   python3 "${DIR}/move_data/app.py"
   python3 "${DIR}/post_migration_db_tasks/app.py"
 else
