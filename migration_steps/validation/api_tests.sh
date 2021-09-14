21dc46db7f1fa1c36c1fd1a451ece711df4023bb
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2021-09-14 14:15:32 +0100

    todos

--- migration_steps/validation/api_tests.sh
+++ migration_steps/validation/api_tests.sh
@@ -2,6 +2,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# todo IN-908
 if [[ "${ENVIRONMENT}" == "local" || "${ENVIRONMENT}" == "development" ]]
 then
   python3 "${DIR}/api_test_data_s3_upload/app/app.py"
--- migration_steps/validation/functional_api_tests.sh
+++ migration_steps/validation/functional_api_tests.sh
@@ -2,6 +2,7 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# todo IN-908
 if [[ "${ENVIRONMENT}" == "local" || "${ENVIRONMENT}" == "development" ]]
 then
   if [ "${RUN_API_TESTS}" == "True" ]
