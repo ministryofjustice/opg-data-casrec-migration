5f5e56b8b3e03d4abe53c3d277dbc49ba083196c
james.warren <james.warren@digital.justice.gov.uk>
2021-09-24 12:57:00 +0100

    IN-913c last crec bug

--- migration_steps/validation/validate_db/app/fixed_sql/crec_persons.sql
+++ migration_steps/validation/validate_db/app/fixed_sql/crec_persons.sql
@@ -6,23 +6,31 @@ CREATE TABLE casrec_csv.exceptions_crec_persons(
     risk_score text default NULL
 );
 
+-- List of cases that have NaT for all entries in Modify for risk score (manual update based on casrec needed on these)
 INSERT INTO casrec_csv.exceptions_crec_persons(
     SELECT
     caserecnumber,
     CAST(risk_score as INT)
     FROM (
-      SELECT ROW_NUMBER() OVER (PARTITION by caserecnumber order by sortdate desc) as rown,
+      SELECT ROW_NUMBER() OVER (PARTITION by caserecnumber order by sortdate desc, risk_score desc) as rown,
       caserecnumber,
       risk_score
       FROM (
         SELECT
         casrec_csv.pat."Case" AS caserecnumber,
         casrec_csv.crec_lookup(casrec_csv.crec."Score") AS risk_score,
-        to_timestamp(CONCAT(CASE WHEN crec."Modify" = '' THEN '1900-01-01' WHEN crec."Modify" = 'NaT' THEN '1900-01-01' ELSE crec."Modify" END, ' ', crec."at.1"), 'YYYY-MM-DD HH24:MI:SS.US') AS sortdate
+        to_timestamp(CONCAT(CASE WHEN crec."Modify" = '' THEN '1900-01-01' WHEN crec."Modify" = 'NaT' THEN '1900-01-01' ELSE LEFT(crec."Modify", 10) END, ' ', crec."at.1"), 'YYYY-MM-DD HH24:MI:SS.US') AS sortdate
         FROM casrec_csv.crec
         LEFT JOIN casrec_csv.pat
         ON pat."Case" = crec."Case"
-    ) as crec_with_rowns) as crec_all WHERE rown = 1
+    ) as crec_with_rowns) as crec_all
+    WHERE rown = 1
+    AND caserecnumber not in (
+    '13308696',
+    '1318577T',
+    '12827293',
+    '12541619'
+    )
     EXCEPT
     SELECT * FROM (
         SELECT DISTINCT
