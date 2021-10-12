7444414eaaacd597148e9b80c31bf6fe3e004f9b
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-12 16:39:11 +0100

    Add tasks entity

--- migration_steps/transform_casrec/transform/app/entities/tasks/__init__.py
+++ migration_steps/transform_casrec/transform/app/entities/tasks/__init__.py
@@ -1,10 +1,9 @@
 import logging
 
-
 from helpers import log_title, check_entity_enabled
+from entities.tasks.tasks import insert_tasks
 
 log = logging.getLogger("root")
-
 
 def runner(target_db, db_config):
     """
@@ -17,12 +16,14 @@ def runner(target_db, db_config):
     """
 
     entity_name = "tasks"
-    if not check_entity_enabled(entity_name):
+    extra_entities = ["deputies", "clients"]
+    if not check_entity_enabled(entity_name, extra_entities):
         return False
 
     log.info(log_title(message=entity_name))
 
-    log.debug("Not currently implemented")
+    log.debug("insert_tasks")
+    insert_tasks(target_db=target_db, db_config=db_config, mapping_file="tasks")
 
 
 if __name__ == "__main__":
