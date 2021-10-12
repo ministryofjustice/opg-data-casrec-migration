3924c2a1aed4f5273a39b40b98c0d3925adffae0
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-12 16:39:11 +0100

    Add tasks entity

---
+++ migration_steps/transform_casrec/transform/app/entities/tasks/tasks.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from utilities.basic_data_table import get_basic_data_table
+
+from custom_errors import EmptyDataFrame
+from helpers import get_mapping_dict, get_table_def
+
+
+def insert_tasks(db_config, target_db, mapping_file):
+    chunk_size = db_config["chunk_size"]
+    offset = -chunk_size
+    chunk_no = 0
+
+    mapping_file_name = f"{mapping_file}_mapping"
+    table_definition = get_table_def(mapping_name=mapping_file)
+    sirius_details = get_mapping_dict(
+        file_name=mapping_file_name,
+        stage_name="sirius_details",
+        only_complete_fields=False,
+    )
+
+    while True:
+        offset += chunk_size
+        chunk_no += 1
+
+        try:
+            tasks_df = get_basic_data_table(
+                db_config=db_config,
+                mapping_file_name=mapping_file_name,
+                table_definition=table_definition,
+                sirius_details=sirius_details,
+                chunk_details={"chunk_size": chunk_size, "offset": offset},
+            )
+
+            target_db.insert_data(
+                table_name=table_definition["destination_table_name"],
+                df=tasks_df,
+                sirius_details=sirius_details,
+                chunk_no=chunk_no,
+            )
+
+        except EmptyDataFrame as empty_data_frame:
+            target_db.create_empty_table(sirius_details=sirius_details)
+            break
+
+        except Exception:
+            break
