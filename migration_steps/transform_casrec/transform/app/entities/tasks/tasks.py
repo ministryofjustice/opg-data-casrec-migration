c7e55a08b158bbce899782c0654a8fa6dc2ffdae
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-14 17:30:51 +0100

    Correctly filter by status
    Filter so we only have INACTIVE and ACTIVE status tasks. Then
    reset the status to "Not started" for the tasks which are left
    after filtering.

--- migration_steps/transform_casrec/transform/app/entities/tasks/tasks.py
+++ migration_steps/transform_casrec/transform/app/entities/tasks/tasks.py
@@ -26,6 +26,7 @@ def insert_tasks(db_config, target_db, mapping_file):
         chunk_no += 1
 
         try:
+            # this throws EmptyDataFrame exception if there are no more records
             tasks_df = get_basic_data_table(
                 db_config=db_config,
                 mapping_file_name=mapping_file_name,
@@ -34,20 +35,31 @@ def insert_tasks(db_config, target_db, mapping_file):
                 chunk_details={"chunk_size": chunk_size, "offset": offset},
             )
 
+            num_tasks = len(tasks_df)
+
             # filter so we only have ACTIVE and INACTIVE statuses
             tasks_df = tasks_df[tasks_df['status'].str.contains(r'ACTIVE|INACTIVE')]
 
-            target_db.insert_data(
-                table_name=table_definition["destination_table_name"],
-                df=tasks_df,
-                sirius_details=sirius_details,
-                chunk_no=chunk_no,
-            )
+            # only attempt the insert if we have tasks left to insert
+            if num_tasks > 0:
+                # set status to 'Not started'; note that we have to do this here,
+                # as we need the original status as-is to filter out undesirable records
+                tasks_df['status'] = 'Not started'
 
-        except EmptyDataFrame as empty_data_frame:
+                target_db.insert_data(
+                    table_name=table_definition["destination_table_name"],
+                    df=tasks_df,
+                    sirius_details=sirius_details,
+                    chunk_no=chunk_no,
+                )
+            else:
+                log.info(f'Chunk originally had {num_tasks} tasks, but was filtered to 0; ignoring')
+
+        except EmptyDataFrame:
             log.info('Creating empty tasks table')
             target_db.create_empty_table(sirius_details=sirius_details)
             break
 
-        except Exception:
+        except Exception as e:
+            log.exception(e)
             break
