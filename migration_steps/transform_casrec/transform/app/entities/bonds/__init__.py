eed3ae0b6c67e37eb1e1ec03ef0afd049a520944
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-18 13:25:50 +0100

    Refactor bonds entity to use transformer function

--- migration_steps/transform_casrec/transform/app/entities/bonds/__init__.py
+++ migration_steps/transform_casrec/transform/app/entities/bonds/__init__.py
@@ -1,12 +1,20 @@
 import logging
+import pandas as pd
 
-from entities.bonds.bonds_active import insert_bonds_active
-from entities.bonds.bonds_dispensed import insert_bonds_dispensed
+from entities.bonds.bonds_active import get_bonds_active_chunk
+from entities.bonds.bonds_dispensed import get_bonds_dispensed_chunk
 from helpers import log_title, check_entity_enabled
+from transform_data.transformer import transformer
 
 
 log = logging.getLogger("root")
 
+def _do_existing_cases_prep(db_config, target_db, mapping_file_name):
+    existing_cases_query = f"SELECT c_cop_case, c_bond_no, id from {db_config['target_schema']}.cases;"
+
+    return {
+        'existing_cases_df': pd.read_sql_query(existing_cases_query, db_config["db_connection_string"])
+    }
 
 def runner(target_db, db_config):
     """
@@ -26,15 +34,10 @@ def runner(target_db, db_config):
     log.info(log_title(message=entity_name))
 
     log.debug("insert_bonds_active")
-    insert_bonds_active(
-        target_db=target_db, db_config=db_config, mapping_file="bonds_active"
-    )
+    transformer(db_config, target_db, "bonds_active", get_bonds_active_chunk, _do_existing_cases_prep)
 
     log.debug("insert_bonds_dispensed")
-    insert_bonds_dispensed(
-        target_db=target_db, db_config=db_config, mapping_file="bonds_dispensed"
-    )
-
+    transformer(db_config, target_db, "bonds_dispensed", get_bonds_dispensed_chunk, _do_existing_cases_prep)
 
 if __name__ == "__main__":
 
