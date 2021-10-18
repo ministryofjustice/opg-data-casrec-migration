752c79a4d612e531aa822a5c40e43d2f08625c1f
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-18 13:26:02 +0100

    Refactor cases entity to use transformer function

--- migration_steps/transform_casrec/transform/app/entities/cases/cases.py
+++ migration_steps/transform_casrec/transform/app/entities/cases/cases.py
@@ -1,14 +1,20 @@
-import pandas as pd
+from custom_errors import EmptyDataFrame
 from utilities.basic_data_table import get_basic_data_table
 
-from custom_errors import EmptyDataFrame
-from helpers import get_mapping_dict, get_table_def
 
+def get_cases_chunk(db_config, mapping_file_name, table_definition, sirius_details, chunk_size, offset, prep):
+    try:
+        df = get_basic_data_table(
+            db_config=db_config,
+            mapping_file_name=mapping_file_name,
+            table_definition=table_definition,
+            sirius_details=sirius_details,
+            chunk_details={"chunk_size": chunk_size, "offset": offset},
+        )
 
-def insert_cases(db_config, target_db, mapping_file):
-    chunk_size = db_config["chunk_size"]
-    offset = -chunk_size
-    chunk_no = 0
+        return (df, True)
+    except EmptyDataFrame as e:
+        return (e.df, False)
 
     # persons_query = (
     #     f'select "id", "caserecnumber" from {db_config["target_schema"]}.persons '
@@ -18,51 +24,13 @@ def insert_cases(db_config, target_db, mapping_file):
     #
     # persons_df = persons_df[["id", "caserecnumber"]]
 
-    mapping_file_name = f"{mapping_file}_mapping"
-    table_definition = get_table_def(mapping_name=mapping_file)
-
-    sirius_details = get_mapping_dict(
-        file_name=mapping_file_name,
-        stage_name="sirius_details",
-        only_complete_fields=False,
-    )
-    while True:
-        offset += chunk_size
-        chunk_no += 1
-
-        try:
-            cases_df = get_basic_data_table(
-                db_config=db_config,
-                mapping_file_name=mapping_file_name,
-                table_definition=table_definition,
-                sirius_details=sirius_details,
-                chunk_details={"chunk_size": chunk_size, "offset": offset},
-            )
-
-            # cases_joined_df = cases_df.merge(
-            #     persons_df,
-            #     how="left",
-            #     left_on="caserecnumber",
-            #     right_on="caserecnumber",
-            # )
-            #
-            # cases_joined_df["client_id"] = cases_joined_df["id_y"]
-            # cases_joined_df = cases_joined_df.drop(columns=["id_y"])
-            # cases_joined_df = cases_joined_df.rename(columns={"id_x": "id"})
-
-            if len(cases_df) > 0:
-
-                target_db.insert_data(
-                    table_name=table_definition["destination_table_name"],
-                    df=cases_df,
-                    sirius_details=sirius_details,
-                )
-
-        except EmptyDataFrame as empty_data_frame:
-            if empty_data_frame.empty_data_frame_type == "chunk":
-                target_db.create_empty_table(sirius_details=sirius_details)
-                break
-            continue
-
-        except Exception:
-            break
+    # cases_joined_df = cases_df.merge(
+    #     persons_df,
+    #     how="left",
+    #     left_on="caserecnumber",
+    #     right_on="caserecnumber",
+    # )
+    #
+    # cases_joined_df["client_id"] = cases_joined_df["id_y"]
+    # cases_joined_df = cases_joined_df.drop(columns=["id_y"])
+    # cases_joined_df = cases_joined_df.rename(columns={"id_x": "id"})
