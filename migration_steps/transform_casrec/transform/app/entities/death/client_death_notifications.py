ab372f22a4ea42698518d3615eb588d2adcbba8f
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-18 14:16:14 +0100

    Refactor death entity to use transformer

--- migration_steps/transform_casrec/transform/app/entities/death/client_death_notifications.py
+++ migration_steps/transform_casrec/transform/app/entities/death/client_death_notifications.py
@@ -6,64 +6,48 @@ from transform_data.apply_datatypes import reapply_datatypes_to_fk_cols
 from utilities.basic_data_table import get_basic_data_table
 
 
-def insert_client_death_notifications(db_config, target_db, mapping_file):
-    chunk_size = db_config["chunk_size"]
-    offset = -chunk_size
-    chunk_no = 0
-
-    persons_query = (
+def do_clients_prep(db_config, target_db, mapping_file_name):
+    clients_query = (
         f'select "id", "caserecnumber" from {db_config["target_schema"]}.persons '
         f"where \"type\" = 'actor_client';"
     )
-    persons_df = pd.read_sql_query(persons_query, db_config["db_connection_string"])
 
-    persons_df = persons_df[["id", "caserecnumber"]]
+    return {
+        'clients_df': pd.read_sql_query(clients_query, db_config["db_connection_string"])
+    }
 
-    mapping_file_name = f"{mapping_file}_mapping"
-    table_definition = get_table_def(mapping_name=mapping_file)
+def get_client_death_notifications_chunk(
+        db_config,
+        mapping_file_name,
+        table_definition,
+        sirius_details,
+        chunk_size,
+        offset,
+        prep
+):
+    try:
+        client_death_df = get_basic_data_table(
+            db_config=db_config,
+            mapping_file_name=mapping_file_name,
+            table_definition=table_definition,
+            sirius_details=sirius_details,
+            chunk_details={"chunk_size": chunk_size, "offset": offset},
+        )
+    except EmptyDataFrame as e:
+        return (e.df, False)
 
-    sirius_details = get_mapping_dict(
-        file_name=mapping_file_name,
-        stage_name="sirius_details",
-        only_complete_fields=False,
+    clients_df = prep['clients_df']
+
+    death_joined_df = client_death_df.merge(
+        clients_df, how="left", left_on="c_case", right_on="caserecnumber"
     )
-    while True:
-        offset += chunk_size
-        chunk_no += 1
 
-        try:
-            client_death_df = get_basic_data_table(
-                db_config=db_config,
-                mapping_file_name=mapping_file_name,
-                table_definition=table_definition,
-                sirius_details=sirius_details,
-                chunk_details={"chunk_size": chunk_size, "offset": offset},
-            )
+    death_joined_df["person_id"] = death_joined_df["id_y"]
+    death_joined_df = death_joined_df.drop(columns=["id_y"])
+    death_joined_df = death_joined_df.rename(columns={"id_x": "id"})
 
-            death_joined_df = client_death_df.merge(
-                persons_df, how="left", left_on="c_case", right_on="caserecnumber"
-            )
+    death_joined_df = reapply_datatypes_to_fk_cols(
+        columns=["person_id"], df=death_joined_df
+    )
 
-            death_joined_df["person_id"] = death_joined_df["id_y"]
-            death_joined_df = death_joined_df.drop(columns=["id_y"])
-            death_joined_df = death_joined_df.rename(columns={"id_x": "id"})
-
-            death_joined_df = reapply_datatypes_to_fk_cols(
-                columns=["person_id"], df=death_joined_df
-            )
-
-            target_db.insert_data(
-                table_name=table_definition["destination_table_name"],
-                df=death_joined_df,
-                sirius_details=sirius_details,
-                chunk_no=chunk_no,
-            )
-
-        except EmptyDataFrame as empty_data_frame:
-            if empty_data_frame.empty_data_frame_type == 'chunk':
-                target_db.create_empty_table(sirius_details=sirius_details)
-                break
-            continue
-
-        except Exception:
-            break
+    return (death_joined_df, True)
