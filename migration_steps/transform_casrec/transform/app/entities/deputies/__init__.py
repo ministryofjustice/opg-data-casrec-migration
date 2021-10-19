c83b77c0c1efb26f54e7fadaae46a68e74710fb9
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-19 08:21:23 +0100

    Refactor deputies other tables to use transformer

--- migration_steps/transform_casrec/transform/app/entities/deputies/__init__.py
+++ migration_steps/transform_casrec/transform/app/entities/deputies/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
-from entities.deputies.order_deputy import insert_order_deputies
-from entities.deputies.addresses import insert_addresses_deputies
-from entities.deputies.persons import insert_persons_deputies
+from entities.deputies.addresses import get_addresses_deputies_chunk, do_addresses_deputies_prep
+from entities.deputies.order_deputy import get_order_deputies_records
+from entities.deputies.persons import get_persons_deputies_chunk
 from entities.deputies.phonenumbers import get_phonenumbers_deputies_chunk, do_deputies_prep
 from helpers import log_title, check_entity_enabled
 from transform_data.transformer import transformer
@@ -28,9 +28,7 @@ def runner(target_db, db_config):
     log.info(log_title(message=entity_name))
 
     log.debug("insert_persons_deputies")
-    insert_persons_deputies(
-        target_db=target_db, db_config=db_config, mapping_file="deputy_persons"
-    )
+    transformer(db_config, target_db, "deputy_persons", get_persons_deputies_chunk)
 
     log.debug("insert_phonenumbers_deputies - daytime")
     transformer(
@@ -51,18 +49,16 @@ def runner(target_db, db_config):
     )
 
     log.debug("insert_addresses_deputies")
-    insert_addresses_deputies(
-        mapping_file="deputy_addresses",
-        target_db=target_db,
-        db_config=db_config,
+    transformer(
+        db_config,
+        target_db,
+        "deputy_addresses",
+        get_addresses_deputies_chunk,
+        do_addresses_deputies_prep
     )
 
     log.debug("insert_order_deputies")
-    insert_order_deputies(
-        mapping_file="order_deputy",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "order_deputy", get_order_deputies_records)
 
 
 if __name__ == "__main__":
