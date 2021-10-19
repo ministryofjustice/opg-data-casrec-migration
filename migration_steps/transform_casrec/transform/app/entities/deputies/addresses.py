fe2b3e18dadea9af88ad1e3b2ed3aa8747d87474
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-19 08:21:23 +0100

    Refactor deputies other tables to use transformer

--- migration_steps/transform_casrec/transform/app/entities/deputies/addresses.py
+++ migration_steps/transform_casrec/transform/app/entities/deputies/addresses.py
@@ -1,22 +1,18 @@
+import pandas as pd
+
 from custom_errors import EmptyDataFrame
-from helpers import get_mapping_dict, get_table_def
+from transform_data import unique_id as process_unique_id
 from transform_data.apply_datatypes import reapply_datatypes_to_fk_cols
 from utilities.basic_data_table import get_basic_data_table
-import pandas as pd
-from transform_data import unique_id as process_unique_id
 from utilities.df_helpers import prep_df_for_merge
 
 
-def insert_addresses_deputies(db_config, target_db, mapping_file):
-
+def do_addresses_deputies_prep(db_config, target_db, mapping_file_name):
     deputyship_query = f"""
         select "Dep Addr No", "Deputy No"
         from {db_config['source_schema']}.deputyship
     """
-    deputyship_df = pd.read_sql_query(
-        deputyship_query, db_config["db_connection_string"]
-    )
-
+    deputyship_df = pd.read_sql_query(deputyship_query, db_config["db_connection_string"])
     deputyship_df = prep_df_for_merge(df=deputyship_df, column="Dep Addr No")
 
     deputy_persons_query = f"""
@@ -24,96 +20,75 @@ def insert_addresses_deputies(db_config, target_db, mapping_file):
         from {db_config['target_schema']}.persons
         where casrec_mapping_file_name = 'deputy_persons_mapping'
     """
+    deputy_persons_df = pd.read_sql_query(deputy_persons_query, db_config["db_connection_string"])
 
-    deputy_persons_df = pd.read_sql_query(
-        deputy_persons_query, db_config["db_connection_string"]
+    return {
+        'deputyship_df': deputyship_df,
+        'deputy_persons_df': deputy_persons_df
+    }
+
+def get_addresses_deputies_chunk(db_config, mapping_file_name, table_definition, sirius_details, chunk_size, offset, prep):
+    try:
+        addresses_df = get_basic_data_table(
+            db_config=db_config,
+            mapping_file_name=mapping_file_name,
+            table_definition=table_definition,
+            sirius_details=sirius_details,
+            chunk_details={"chunk_size": chunk_size, "offset": offset},
+        )
+    except EmptyDataFrame as e:
+        more_records = (e.empty_data_frame_type != 'chunk')
+        return (e.df, more_records)
+
+    deputyship_df = prep['deputyship_df']
+    deputy_persons_df = prep['deputy_persons_df']
+
+    addresses_df = prep_df_for_merge(df=addresses_df, column="c_dep_addr_no")
+
+    address_deputyship_joined_df = addresses_df.merge(
+        deputyship_df,
+        how="left",
+        left_on="c_dep_addr_no",
+        right_on="Dep Addr No",
     )
 
-    chunk_size = db_config["chunk_size"]
-    offset = -chunk_size
-    chunk_no = 0
-
-    mapping_file_name = f"{mapping_file}_mapping"
-    table_definition = get_table_def(mapping_name=mapping_file)
-
-    sirius_details = get_mapping_dict(
-        file_name=mapping_file_name,
-        stage_name="sirius_details",
-        only_complete_fields=False,
+    address_persons_joined_df = address_deputyship_joined_df.merge(
+        deputy_persons_df,
+        how="left",
+        left_on="Deputy No",
+        right_on="c_deputy_no",
     )
-    while True:
-        offset += chunk_size
-        chunk_no += 1
 
-        try:
-            addresses_df = get_basic_data_table(
-                db_config=db_config,
-                mapping_file_name=mapping_file_name,
-                table_definition=table_definition,
-                sirius_details=sirius_details,
-                chunk_details={"chunk_size": chunk_size, "offset": offset},
-            )
+    address_persons_joined_df = address_persons_joined_df.drop(
+        columns=["Dep Addr No", "Deputy No"]
+    )
 
-            addresses_df = prep_df_for_merge(df=addresses_df, column="c_dep_addr_no")
+    address_persons_joined_df["person_id"] = (
+        address_persons_joined_df["person_id"]
+        .fillna(0)
+        .astype(int)
+        .astype(object)
+        .where(address_persons_joined_df["person_id"].notnull())
+    )
 
-            address_deputyship_joined_df = addresses_df.merge(
-                deputyship_df,
-                how="left",
-                left_on="c_dep_addr_no",
-                right_on="Dep Addr No",
-            )
+    address_persons_joined_df = address_persons_joined_df.drop_duplicates()
 
-            address_persons_joined_df = address_deputyship_joined_df.merge(
-                deputy_persons_df,
-                how="left",
-                left_on="Deputy No",
-                right_on="c_deputy_no",
-            )
+    address_persons_joined_df = address_persons_joined_df.drop(columns=["id"])
 
-            address_persons_joined_df = address_persons_joined_df.drop(
-                columns=["Dep Addr No", "Deputy No"]
-            )
+    address_persons_joined_df = process_unique_id.add_unique_id(
+        db_conn_string=db_config["db_connection_string"],
+        db_schema=db_config["target_schema"],
+        table_definition=table_definition,
+        source_data_df=address_persons_joined_df,
+    )
 
-            address_persons_joined_df["person_id"] = (
-                address_persons_joined_df["person_id"]
-                .fillna(0)
-                .astype(int)
-                .astype(object)
-                .where(address_persons_joined_df["person_id"].notnull())
-            )
+    # some addresses don't seem to match up with people...
+    address_persons_joined_df = address_persons_joined_df[
+        address_persons_joined_df["person_id"].notna()
+    ]
 
-            address_persons_joined_df = address_persons_joined_df.drop_duplicates()
+    address_persons_joined_df = reapply_datatypes_to_fk_cols(
+        columns=["person_id"], df=address_persons_joined_df
+    )
 
-            address_persons_joined_df = address_persons_joined_df.drop(columns=["id"])
-
-            address_persons_joined_df = process_unique_id.add_unique_id(
-                db_conn_string=db_config["db_connection_string"],
-                db_schema=db_config["target_schema"],
-                table_definition=table_definition,
-                source_data_df=address_persons_joined_df,
-            )
-
-            # some addresses don't seem to match up with people...
-            address_persons_joined_df = address_persons_joined_df[
-                address_persons_joined_df["person_id"].notna()
-            ]
-
-            address_persons_joined_df = reapply_datatypes_to_fk_cols(
-                columns=["person_id"], df=address_persons_joined_df
-            )
-
-            target_db.insert_data(
-                table_name=table_definition["destination_table_name"],
-                df=address_persons_joined_df,
-                sirius_details=sirius_details,
-                chunk_no=chunk_no,
-            )
-
-        except EmptyDataFrame as empty_data_frame:
-            if empty_data_frame.empty_data_frame_type == 'chunk':
-                target_db.create_empty_table(sirius_details=sirius_details)
-                break
-            continue
-
-        except Exception:
-            break
+    return (address_persons_joined_df, True)
