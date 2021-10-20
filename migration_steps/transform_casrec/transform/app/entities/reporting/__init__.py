82e92169146565489bc65fc0861111a82908c043
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-20 10:07:34 +0100

    Refactor reporting to use transformer function

--- migration_steps/transform_casrec/transform/app/entities/reporting/__init__.py
+++ migration_steps/transform_casrec/transform/app/entities/reporting/__init__.py
@@ -3,11 +3,14 @@ import logging
 from entities.reporting.annual_report_lodging_details import (
     insert_annual_report_lodging_details,
 )
-from entities.reporting.annual_report_logs import insert_annual_report_logs
+from entities.reporting.annual_report_logs import (
+    get_annual_report_logs_chunk, do_annual_report_logs_prep,
+)
 from helpers import log_title, check_entity_enabled
+from transform_data.transformer import transformer
+
 
 log = logging.getLogger("root")
-
 
 def runner(target_db, db_config):
     """
@@ -26,10 +29,12 @@ def runner(target_db, db_config):
     log.info(log_title(message=entity_name))
 
     log.debug("insert_annual_report_logs")
-    insert_annual_report_logs(
-        mapping_file="annual_report_logs",
-        target_db=target_db,
-        db_config=db_config,
+    transformer(
+        db_config,
+        target_db,
+        "annual_report_logs",
+        get_annual_report_logs_chunk,
+        do_annual_report_logs_prep
     )
 
     log.debug("insert_annual_report_lodging_details")
