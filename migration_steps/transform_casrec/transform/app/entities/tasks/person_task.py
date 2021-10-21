6eee50772ffe53733f30116bfb6c6aca8369f52d
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-21 18:10:07 +0100

    IN-472 Tasks migration scripts (#382)
    * Use binary package for psycopg2
    
    This removes the need to compile this package, which fails on
    my machine anyway.
    
    * Add tasks entity
    
    * Include tasks transforms in local runs
    
    * Add mapping for person_task
    
    * Filter out tasks with status other than ACTIVE or INACTIVE
    
    * Additional log statements to track what's happening to transform tables
    
    * Correctly filter by status
    
    Filter so we only have INACTIVE and ACTIVE status tasks. Then
    reset the status to "Not started" for the tasks which are left
    after filtering.
    
    * Fix the conditional for deciding whether to do the db_insert for tasks
    
    * Improve/reduce comments
    
    * Add an include_values condition
    
    This allows filtering to leave records whose fields match the
    include_values conditions. The main use case is for tasks,
    where we want tasks with a Status of ACTIVE or INACTIVE.
    
    * Remove filtering by status from transform script
    
    As the filter is now specified in the spreadsheet, remove
    manual filtering which was applied here.
    
    * Create table if EmptyDataFrame exception was raised during chunking
    
    Co-authored-by: Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
    
    * Use binary package for psycopg2
    
    This removes the need to compile this package, which fails on
    my machine anyway.
    
    * Add tasks entity
    
    * Include tasks transforms in local runs
    
    * Add mapping for person_task
    
    * Filter out tasks with status other than ACTIVE or INACTIVE
    
    * Additional log statements to track what's happening to transform tables
    
    * Correctly filter by status
    
    Filter so we only have INACTIVE and ACTIVE status tasks. Then
    reset the status to "Not started" for the tasks which are left
    after filtering.
    
    * Fix the conditional for deciding whether to do the db_insert for tasks
    
    * Improve/reduce comments
    
    * Add an include_values condition
    
    This allows filtering to leave records whose fields match the
    include_values conditions. The main use case is for tasks,
    where we want tasks with a Status of ACTIVE or INACTIVE.
    
    * Remove filtering by status from transform script
    
    As the filter is now specified in the spreadsheet, remove
    manual filtering which was applied here.
    
    * Create table if EmptyDataFrame exception was raised during chunking
    
    Co-authored-by: Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
    
    * Remove misleading SQL
    
    There is no need for an assignee with ID 1, so remove bogus line
    which makes it look like there could be.
    
    * Add tasks entity tables to integration
    
    Co-authored-by: Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>

---
+++ migration_steps/transform_casrec/transform/app/entities/tasks/person_task.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from helpers import get_mapping_dict, get_table_def
+
+def insert_person_task(db_config, target_db, mapping_file):
+    sirius_details = get_mapping_dict(
+        file_name=f"{mapping_file}_mapping",
+        stage_name="sirius_details",
+        only_complete_fields=False,
+    )
+
+    persons_query = (
+        f"select id as person_id, caserecnumber from {db_config['target_schema']}.persons "
+        f"where type = 'actor_client';"
+    )
+    persons_df = pd.read_sql_query(persons_query, db_config["db_connection_string"])
+
+    tasks_query = f'select id as task_id, c_case from {db_config["target_schema"]}.tasks;'
+    tasks_df = pd.read_sql_query(tasks_query, db_config["db_connection_string"])
+
+    person_task_df = tasks_df.merge(
+        persons_df,
+        how="left",
+        left_on="c_case",
+        right_on="caserecnumber",
+    )
+
+    person_task_df = person_task_df[['person_id', 'task_id']]
+    person_task_df["casrec_details"] = "{}"
+
+    table_definition = get_table_def(mapping_name=mapping_file)
+    target_db.insert_data(
+        table_name=table_definition["destination_table_name"],
+        df=person_task_df,
+        sirius_details=sirius_details,
+    )
