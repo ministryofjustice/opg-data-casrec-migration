db2ec340099983d72ba98609b6f54c89aa23548d
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-22 08:11:02 +0100

    Refactor tasks to use transformer function

--- migration_steps/transform_casrec/transform/app/entities/tasks/person_task.py
+++ migration_steps/transform_casrec/transform/app/entities/tasks/person_task.py
@@ -1,14 +1,7 @@
 import pandas as pd
 
-from helpers import get_mapping_dict, get_table_def
 
-def insert_person_task(db_config, target_db, mapping_file):
-    sirius_details = get_mapping_dict(
-        file_name=f"{mapping_file}_mapping",
-        stage_name="sirius_details",
-        only_complete_fields=False,
-    )
-
+def get_person_task_records(db_config, mapping_file_name, table_definition, sirius_details, chunk_size, offset, prep):
     persons_query = (
         f"select id as person_id, caserecnumber from {db_config['target_schema']}.persons "
         f"where type = 'actor_client';"
@@ -28,9 +21,4 @@ def insert_person_task(db_config, target_db, mapping_file):
     person_task_df = person_task_df[['person_id', 'task_id']]
     person_task_df["casrec_details"] = "{}"
 
-    table_definition = get_table_def(mapping_name=mapping_file)
-    target_db.insert_data(
-        table_name=table_definition["destination_table_name"],
-        df=person_task_df,
-        sirius_details=sirius_details,
-    )
+    return (person_task_df, False)
