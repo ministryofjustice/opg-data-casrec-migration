b04caf5820bd3dce78498418685d4c39c13f72b0
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-22 09:40:47 +0100

    Refactor warnings to use transformer function

--- migration_steps/transform_casrec/transform/app/entities/warnings/__init__.py
+++ migration_steps/transform_casrec/transform/app/entities/warnings/__init__.py
@@ -1,19 +1,29 @@
 import logging
 
-from entities.warnings.client_nodebtchase_warnings import (
-    insert_client_nodebtchase_warnings,
-)
-from entities.warnings.client_person_warning import insert_client_person_warning
-from entities.warnings.client_saarcheck_warnings import insert_client_saarcheck_warnings
-from entities.warnings.client_special_warnings import insert_client_special_warnings
-from entities.warnings.client_violent_warnings import insert_client_violent_warnings
-from entities.warnings.deputy_person_warning import insert_deputy_person_warning
-from entities.warnings.deputy_special_warnings import insert_deputy_special_warnings
-from entities.warnings.deputy_violent_warnings import insert_deputy_violent_warnings
+from custom_errors import EmptyDataFrame
+from entities.warnings.client_person_warning import get_client_person_warning_records
+from entities.warnings.deputy_person_warning import get_deputy_person_warning_records
 from helpers import log_title, check_entity_enabled
+from transform_data.transformer import transformer
+from utilities.basic_data_table import get_basic_data_table
+
 
 log = logging.getLogger("root")
 
+def _get_warnings_chunk(db_config, mapping_file_name, table_definition, sirius_details, chunk_size, offset, prep):
+    try:
+        warnings_df = get_basic_data_table(
+            db_config=db_config,
+            mapping_file_name=mapping_file_name,
+            table_definition=table_definition,
+            sirius_details=sirius_details,
+            chunk_details={"chunk_size": chunk_size, "offset": offset},
+        )
+
+        return (warnings_df, True)
+    except EmptyDataFrame as e:
+        more_records = (e.empty_data_frame_type != 'chunk')
+        return (e.df, more_records)
 
 def runner(target_db, db_config):
     """
@@ -33,56 +43,28 @@ def runner(target_db, db_config):
     log.info(log_title(message=entity_name))
 
     log.debug("insert_client_violent_warnings")
-    insert_client_violent_warnings(
-        mapping_file="client_violent_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
-    log.debug("insert_client_special_warnings")
-    insert_client_special_warnings(
-        mapping_file="client_special_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "client_violent_warnings", _get_warnings_chunk)
+
     log.debug("insert_client_saarcheck_warnings")
-    insert_client_saarcheck_warnings(
-        mapping_file="client_saarcheck_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "client_saarcheck_warnings", _get_warnings_chunk)
+
+    log.debug("insert_client_special_warnings")
+    transformer(db_config, target_db, "client_special_warnings", _get_warnings_chunk)
+
     log.debug("insert_client_nodebtchase_warnings")
-    insert_client_nodebtchase_warnings(
-        mapping_file="client_nodebtchase_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "client_nodebtchase_warnings", _get_warnings_chunk)
 
     log.debug("insert_client_person_warning")
-    insert_client_person_warning(
-        mapping_file="person_warning",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "person_warning", get_client_person_warning_records)
 
     log.debug("insert_deputy_violent_warnings")
-    insert_deputy_violent_warnings(
-        mapping_file="deputy_violent_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "deputy_violent_warnings", _get_warnings_chunk)
+
     log.debug("insert_deputy_special_warnings")
-    insert_deputy_special_warnings(
-        mapping_file="deputy_special_warnings",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "deputy_special_warnings", _get_warnings_chunk)
 
     log.debug("insert_deputy_person_warning")
-    insert_deputy_person_warning(
-        mapping_file="person_warning",
-        target_db=target_db,
-        db_config=db_config,
-    )
+    transformer(db_config, target_db, "person_warning", get_deputy_person_warning_records)
 
 
 if __name__ == "__main__":
--- migration_steps/transform_casrec/transform/app/entities/warnings/client_person_warning.py
+++ migration_steps/transform_casrec/transform/app/entities/warnings/client_person_warning.py
@@ -1,85 +1,53 @@
-import logging
-
 import pandas as pd
 
-from custom_errors import EmptyDataFrame
-from helpers import get_mapping_dict, format_error_message, get_table_def
 from transform_data.apply_datatypes import reapply_datatypes_to_fk_cols
 
-log = logging.getLogger("root")
 
-# definition = {
-#     "destination_table_name": "person_warning",
-#     "source_table_name": "",
-#     "source_table_additional_columns": [],
-# }
-#
-# mapping_file_name = "person_warning_mapping"
+def get_client_person_warning_records(
+    db_config,
+    mapping_file_name,
+    table_definition,
+    sirius_details,
+    chunk_size,
+    offset,
+    prep
+):
+    client_warning_query = f'select * from {db_config["target_schema"]}.warnings;'
 
-
-def insert_client_person_warning(db_config, target_db, mapping_file):
-
-    mapping_file_name = f"{mapping_file}_mapping"
-    table_definition = get_table_def(mapping_name=mapping_file)
-    sirius_details = get_mapping_dict(
-        file_name=mapping_file_name,
-        stage_name="sirius_details",
-        only_complete_fields=False,
+    client_warning_df = pd.read_sql_query(
+        client_warning_query, db_config["db_connection_string"]
     )
 
-    try:
+    if len(client_warning_df) == 0:
+        return (client_warning_df, False)
 
-        clients_query = (
-            f'select "id", "caserecnumber" from {db_config["target_schema"]}.persons '
-            f"where \"type\" = 'actor_client';"
-        )
-        clients_df = pd.read_sql_query(clients_query, db_config["db_connection_string"])
+    clients_query = (
+        f'select "id", "caserecnumber" from {db_config["target_schema"]}.persons '
+        f"where \"type\" = 'actor_client';"
+    )
 
-        client_warning_query = f'select * from {db_config["target_schema"]}.warnings;'
-        client_warning_df = pd.read_sql_query(
-            client_warning_query, db_config["db_connection_string"]
-        )
-        if len(client_warning_df) == 0:
-            raise EmptyDataFrame
+    clients_df = pd.read_sql_query(clients_query, db_config["db_connection_string"])
 
-        client_warning_df = client_warning_df[["id", "c_case"]]
+    client_warning_df = client_warning_df[["id", "c_case"]]
 
-        client_warning_df = client_warning_df.merge(
-            clients_df,
-            how="inner",
-            left_on="c_case",
-            right_on="caserecnumber",
-            suffixes=["_warning", "_client"],
-        )
+    client_warning_df = client_warning_df.merge(
+        clients_df,
+        how="inner",
+        left_on="c_case",
+        right_on="caserecnumber",
+        suffixes=["_warning", "_client"],
+    )
 
-        client_warning_df = client_warning_df.drop(columns=["caserecnumber"])
-        client_warning_df = client_warning_df.rename(
-            columns={"id_warning": "warning_id", "id_client": "person_id"}
-        )
-        client_warning_df["casrec_details"] = "{}"
+    client_warning_df = client_warning_df.drop(columns=["caserecnumber"])
 
-        print(f"len(client_warning_df): {len(client_warning_df)}")
+    client_warning_df = client_warning_df.rename(
+        columns={"id_warning": "warning_id", "id_client": "person_id"}
+    )
 
-        client_warning_df = reapply_datatypes_to_fk_cols(
-            columns=["person_id", "warning_id"], df=client_warning_df
-        )
-        print(f"len(client_warning_df): {len(client_warning_df)}")
+    client_warning_df["casrec_details"] = "{}"
 
-        target_db.insert_data(
-            table_name=table_definition["destination_table_name"],
-            df=client_warning_df,
-            sirius_details=sirius_details,
-        )
-    except EmptyDataFrame:
+    client_warning_df = reapply_datatypes_to_fk_cols(
+        columns=["person_id", "warning_id"], df=client_warning_df
+    )
 
-        target_db.create_empty_table(sirius_details=sirius_details)
-
-    except Exception as e:
-        print(e)
-        log.debug(
-            "No data to insert",
-            extra={
-                "file_name": "",
-                "error": format_error_message(e=e),
-            },
-        )
+    return (client_warning_df, False)
