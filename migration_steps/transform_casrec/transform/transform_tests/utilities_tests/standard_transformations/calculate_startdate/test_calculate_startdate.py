17a905b51702a743b254198e7a3bca6f9243e6c1
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-10-29 11:42:36 +0100

    IN-465 Reporting date calculations (#397)
    * Rework do_calculations()
    
    Functions are not used elsewhere and are trivial, so remove
    that file and associated tests.
    
    The do_calculations() test was hard to follow, so rewrote to properly
    test whether calculations are being applied.
    
    * Put unit tests in for calculate_date field calculation
    
    * Happy path for do_calculations/delta_date calculation
    
    Renamed calculate_date to delta_date, as it makes it more
    obvious what the calculation is doing.
    
    * Unhappy path unit tests for do_calculations/delta_date
    
    * Use correct format for the calculations dict
    
    * Adjust do_calculations to cope with real data
    
    * Move reporting date calculations into standard transformations
    
    * Add script to run unit tests
    
    * Ignore directories called 'build'
    
    This is so I can create artefacts locally (e.g. coverage reports)
    and not accidentally commit them to the repo all the time.
    
    * Ignore docker log files
    
    * Quote column names with spaces
    
    In the case of multiple transformations on a table based off the
    same column (in this case, "End Date"), the CREATE TABLE statement
    is incorrect, as table names are not wrapped in quotes. This
    causes a breakage.
    
    By wrapping all column names in quotes, we ensure that the CREATE
    TABLE statement remains valid.
    
    * Add basic validation SQL for reporting/annual_report_logs
    
    * Remove unused imports
    
    * Remove calculate_reminderdate
    
    As we no longer require the transform for
    annual_report_logs/reportduereminderdate (we're no longer mapping
    this field), remove the associated transform function.

---
+++ migration_steps/transform_casrec/transform/transform_tests/utilities_tests/standard_transformations/calculate_startdate/test_calculate_startdate.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from datetime import datetime
+from pytest_cases import parametrize_with_cases
+
+from utilities.standard_transformations import calculate_startdate
+
+
+# startdate cases
+def case_calculate_startdate():
+    return '2021-10-20', '2020-10-19'
+
+def case_calculate_startdate_datetime():
+    return datetime.strptime('2021-10-20', '%Y-%m-%d'), '2020-10-19'
+
+def case_calculate_startdate_base_not_a_date():
+    return None, None
+
+@parametrize_with_cases("enddate, expected_startdate", cases=".", prefix="case_calculate_startdate")
+def test_calculate_startdate(enddate, expected_startdate):
+    if enddate is not None:
+        enddate = pd.Timestamp(enddate)
+
+    test_data = {
+        'enddate': [enddate, enddate],
+        'startdate': [None, None],
+    }
+
+    test_df = pd.DataFrame(test_data)
+
+    result = calculate_startdate('enddate', 'startdate', test_df)
+
+    for index, row in result.iterrows():
+            if expected_startdate is not None:
+                expected_startdate = pd.Timestamp(expected_startdate)
+            assert row['startdate'] == expected_startdate, f'startdate value was not correct'
