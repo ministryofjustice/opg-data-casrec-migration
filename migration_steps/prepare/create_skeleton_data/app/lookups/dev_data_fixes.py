fadf946674d32584a39305a40a3955db9befecc0
Dimitar Kokov <dimitar.kokov@digital.justice.gov.uk>
2021-11-04 17:00:45 +0000

    IN-767 Assignee mapping for clients and tasks (#418)
    * IN-767 Add assignee lookups to client and task mappings
    
    * IN-767 Add real assignee IDs to local and dev
    
    * IN-767 Copy assignees and bond_providers from Sirius to Integration

--- migration_steps/prepare/create_skeleton_data/app/lookups/dev_data_fixes.py
+++ migration_steps/prepare/create_skeleton_data/app/lookups/dev_data_fixes.py
@@ -11,6 +11,35 @@ from sqlalchemy.sql import text
 log = logging.getLogger("root")
 
 
+def amend_dev_assignees(db_engine):
+    assignee_ids = [2657]
+    assignee_ids.extend(range(2, 510))
+    team_ids = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 109, 119, 141, 193, 229, 266, 267, 268,
+                279, 285, 380, 381, 382, 383, 384, 415, 465, 479, 480]
+    teams = [f"({team_id}, 'Migration Team', 'assignee_team')" for team_id in team_ids]
+    users = [f"({user_id}, 'Migration User', 'assignee_user')" for user_id in assignee_ids if user_id not in team_ids]
+    assignees = teams + users
+
+    sql = f"""
+        INSERT INTO public.assignees (id, name, type)
+        VALUES {",".join(assignees)}
+        ON CONFLICT (id) DO UPDATE
+            SET name = excluded.name,
+                type = excluded.type;
+    """
+
+    try:
+        db_engine.execute(sql)
+
+    except Exception as e:
+        log.error(
+            f"Unable to insert/update assignees in Sirius DB",
+            extra={
+                "error": format_error_message(e=e),
+            },
+        )
+
+
 def amend_dev_data(db_engine):
     log.info(
         "Amending Dev Sirius DB to match preprod - this should NOT run on preprod!"
@@ -22,7 +51,6 @@ def amend_dev_data(db_engine):
         sql = text(sql_file.read())
 
     try:
-
         db_engine.execute(sql)
 
     except Exception as e:
@@ -34,3 +62,5 @@ def amend_dev_data(db_engine):
                 "error": format_error_message(e=e),
             },
         )
+
+    amend_dev_assignees(db_engine=db_engine)
