0b46e3b61f75e78284a04c111ecd7dbcc69b8bec
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-11-08 16:19:27 +0000

    Extract table transform for annual_report_logs into new file

--- migration_steps/transform_casrec/transform/app/transform_data/table_transforms.py
+++ migration_steps/transform_casrec/transform/app/transform_data/table_transforms.py
@@ -3,6 +3,49 @@ Table transforms are applied to convert one or more columns in the source
 CSV into one or more columns in the target table. At the point of transformation,
 datatypes have been applied to the source CSV, so date comparisons etc. can be
 used in the transforms.
+
+Each table transform is defined in a file which exports a constant representing
+the table transform configuration, as follows:
+
+{
+    'datatypes': <dict of column name to datatype pairs>,
+    'mappings': <list of mapping dicts>,
+    'local_vars': <dict of variables interpolated into mapping dicts>
+}
+
+# datatypes
+
+Columns in the dataframe are cast to the specified datatypes before the mappings
+are applied. This dict maps from column name to datatype; datatype is one of
+the strings recognised
+
+# mappings
+
+Each mapping dict in this list specifies either:
+
+* default_cols: default values for the specified columns before
+  more-specific transforms are applied; this ensures these columns
+  get a value in rows which don't match the more-specific transforms
+
+* a dict with 'criteria' (and-ed together to construct a dataframe query)
+  and 'output_cols' keys.
+
+  criteria may be inline clauses for the dataframe query, or a combination
+  of operator (see below) with a list of fields:
+
+  * all_null: True if all the specified columns have null (or NaT) values
+  * any_not_null: True if one or more of the specified columns are not null/NaT
+
+  output_cols: columns to set on a row with specified values if criteria are met
+
+  any '@' variables in these criteria must be set in the local_vars dict and
+  passed into the apply_table_transformation() function
+
+# local_vars
+
+Variables which are to be interpolated into mappings when constructing queries on an
+annual_report_logs dataframe. These are referenced using the pandas "@" syntax
+within the mappings list.
 """
 
 import logging
@@ -11,6 +54,7 @@ import pandas as pd
 from datetime import datetime
 
 from transform_data.apply_datatypes import apply_datatypes
+from transform_data.table_transforms_annual_report_logs import TABLE_TRANSFORM_ANNUAL_REPORT_LOGS
 
 
 log = logging.getLogger('root')
@@ -63,192 +107,6 @@ def apply_table_transformation(df: pd.DataFrame, mapping: dict, local_vars: dict
     return df
 
 
-# all received date columns; these are used to check whether there
-# is at least one received date
-_rcvd_date_cols = [
-    'c_rcvd_date',
-    'c_rcvd_date1',
-    'c_rcvd_date2',
-    'c_rcvd_date3',
-    'c_rcvd_date4',
-    'c_rcvd_date5',
-    'c_rcvd_date6'
-]
-
-# date columns we check for null values
-_all_null_date_cols = _rcvd_date_cols + [
-    'c_lodge_date',
-    'c_review_date'
-]
-
-# map of date column names to data type, so we can apply correct datatypes
-# to dates for the purpose of doing comparisons below
-_aliased_column_datatypes = {}
-for date_col in _all_null_date_cols + ['c_end_date']:
-    _aliased_column_datatypes[date_col] = {'data_type': 'date'}
-
-# default_cols: specifies default values for columns mentioned before
-#     more-specific transforms are applied; this ensures these fields
-#     get a value in rows which don't match the more-specific transforms
-#
-# criteria are and-ed together
-# all_null: True if all the specified columns have null (or NaT) values
-# any_not_null: True if one or more of the specified columns are not null/NaT
-#
-# output_cols: columns to set on a row with specified values if criteria are met
-#
-# any '@' variables in these criteria must be set in the local_vars dict and
-# passed into the apply_table_transformation() function
-ANNUAL_REPORT_LOGS_STATUS_MAPPINGS = [
-    {
-        'default_cols': {
-            'status': None,
-            'reviewstatus': None
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            'c_end_date > @now',
-            {'all_null': _all_null_date_cols}
-
-        ],
-        'output_cols': {
-            'status': 'PENDING',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            'c_end_date <= @now',
-            'c_end_date > @fifteen_working_days_ago',
-            {'all_null': _all_null_date_cols}
-        ],
-        'output_cols': {
-            'status': 'DUE',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            'c_end_date <= @fifteen_working_days_ago',
-            'c_end_date > @seventy_one_working_days_ago',
-            {'all_null': _all_null_date_cols}
-        ],
-        'output_cols': {
-            'status': 'OVERDUE',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            'c_end_date <= @seventy_one_working_days_ago',
-            {'all_null': _all_null_date_cols}
-        ],
-        'output_cols': {
-            'status': 'NON_COMPLIANT',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            {'any_not_null': _rcvd_date_cols},
-            {'all_null': ['c_lodge_date', 'c_review_date']}
-        ],
-        'output_cols': {
-            'status': 'RECEIVED',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "N"',
-            {'any_not_null': _rcvd_date_cols},
-            {'any_not_null': ['c_lodge_date']},
-            {'all_null': ['c_review_date']}
-        ],
-        'output_cols': {
-            'status': 'LODGED',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "I"',
-            {'any_not_null': _rcvd_date_cols},
-            {'any_not_null': ['c_lodge_date']},
-            {'all_null': ['c_review_date']}
-        ],
-        'output_cols': {
-            'status': 'INCOMPLETE',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "S"',
-            {'any_not_null': _rcvd_date_cols},
-            {'any_not_null': ['c_lodge_date']},
-            {'all_null': ['c_review_date']}
-        ],
-        'output_cols': {
-            'status': 'LODGED',
-            'reviewstatus': 'STAFF_REFERRED'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat.isin(["S", "R", "G", "M"])',
-            {'any_not_null': _rcvd_date_cols},
-            {'any_not_null': ['c_lodge_date']},
-            {'any_not_null': ['c_review_date']}
-        ],
-        'output_cols': {
-            'status': 'LODGED',
-            'reviewstatus': 'REVIEWED'
-        }
-    },
-    {
-        'criteria': [
-            'c_rev_stat == "X"',
-            {'all_null': _all_null_date_cols}
-        ],
-        'output_cols': {
-            'status': 'ABANDONED',
-            'reviewstatus': 'NO_REVIEW'
-        }
-    }
-]
-
-
-def _get_annual_report_logs_local_vars() -> pd.DataFrame:
-    """
-    Get variables which are to be interpolated into mappings when constructing queries on an
-    annual_report_logs dataframe.
-
-    :return: dict of local variables to interpolate into mapping criteria queries
-    """
-
-    # construct variables which are mentioned in the mappings
-    now = np.datetime64(datetime.now().strftime('%Y-%m-%d'))
-
-    # now < End Date + X working days
-    #   is equivalent to
-    # End Date > now - X working days
-    #
-    # roll='forward' means to choose the next working day if the offset
-    # resolves to a day on the weekend
-    return {
-        'now': now,
-        'fifteen_working_days_ago': np.busday_offset(now, -15, roll='forward'),
-        'seventy_one_working_days_ago': np.busday_offset(now, -71, roll='forward')
-    }
-
-
 def _get_annual_report_lodging_details_local_vars() -> pd.DataFrame:
     """
     Get variables which are to be interpolated into mappings when constructing queries on an
@@ -262,15 +120,11 @@ def _get_annual_report_lodging_details_local_vars() -> pd.DataFrame:
 # definition of transforms which may be applied to tables
 #
 # this maps from transform names (as used in mapping spreadsheets) to datatypes, mappings
-# and local variables to be applied
+# and local variables to be applied, typically specified in imported files
 TRANSFORMS = {
-    'set_annual_report_logs_status': {
-        'datatypes': _aliased_column_datatypes,
-        'mappings': ANNUAL_REPORT_LOGS_STATUS_MAPPINGS,
-        'local_vars': _get_annual_report_logs_local_vars()
-    },
+    'set_annual_report_logs_status': TABLE_TRANSFORM_ANNUAL_REPORT_LOGS,
     'set_annual_report_lodging_details_status': {
-        'datatypes': _aliased_column_datatypes,
+        'datatypes': {},
         'mappings': [],
         'local_vars': _get_annual_report_lodging_details_local_vars()
     }
@@ -283,7 +137,8 @@ def process_table_transformations(df: pd.DataFrame, table_transforms: dict) -> p
 
     :param df: dataframe to apply table-level transforms to
     :param table_transforms: dict; Table-level transforms to be applied to the table. This is a
-        map from transform name to parameters for that transform. The parameters are
+        map from transform name to parameters for that transform, derived from the table
+        definitions JSON (which itself comes from the spreadsheets). The parameters are
         primarily informational and used to define the columns required in the initial
         dataframe and hint at which columns will be affected by it, and are not (currently)
         used during transformation.
