204ccab125439e5a99ce42d11a89fc6646495a1c
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2021-11-12 16:58:16 +0000

    Remove unused lookups and incorrect mapping for lodgedstatus

--- migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_lodging_details.sql
+++ migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_lodging_details.sql
@@ -4,7 +4,6 @@ DROP TABLE IF EXISTS casrec_csv.exceptions_annual_report_lodging_details;
 CREATE TABLE casrec_csv.exceptions_annual_report_lodging_details(
     deadlinedate text default NULL,
     datereportlodged text default NULL,
-    lodgedstatus text default NULL,
     bankstatementdeadlinedate text default NULL,
     bankstatementsreceived text default NULL
 );
@@ -13,7 +12,6 @@ INSERT INTO casrec_csv.exceptions_annual_report_lodging_details(
     SELECT
         CAST(NULLIF(account."Followup Date", '') AS date) AS deadlinedate,
         CAST(NULLIF(account."Lodge Date", '') AS date) AS datereportlodged,
-        NULLIF(TRIM(casrec_csv.furthered_lookup(account."Further Code")), '') AS lodgedstatus,
         GREATEST(
             CAST(NULLIF(account."Sent1", '') AS date),
             CAST(NULLIF(account."Sent2", '') AS date),
@@ -42,7 +40,6 @@ INSERT INTO casrec_csv.exceptions_annual_report_lodging_details(
     SELECT
         CAST(annual_report_lodging_details.deadlinedate AS date) AS deadlinedate,
         CAST(annual_report_lodging_details.datereportlodged AS date) AS datereportlodged,
-        NULLIF(TRIM(annual_report_lodging_details.lodgedstatus), '') AS lodgedstatus,
         CAST(annual_report_lodging_details.bankstatementdeadlinedate AS date) AS bankstatementdeadlinedate,
         annual_report_lodging_details.bankstatementsreceived AS bankstatementsreceived
     FROM {target_schema}.annual_report_lodging_details
