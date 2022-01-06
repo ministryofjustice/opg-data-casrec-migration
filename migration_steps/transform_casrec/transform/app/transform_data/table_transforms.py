d6da716feffb55bccac9a51b90d663230705910b
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-01-06 11:17:35 +0000

    Add match_once parameter to table transforms
    (NB this includes lots of fixes from pre-commit hooks)

--- migration_steps/transform_casrec/transform/app/transform_data/table_transforms.py
+++ migration_steps/transform_casrec/transform/app/transform_data/table_transforms.py
@@ -56,19 +56,29 @@ Example:
 {
     'now': <instance of numpy.datetime64>
 }
+
+# match_once
+
+Boolean, default False; if True, once a set of criteria matches a row and transforms it,
+no further matches will be made against that row. This means that the first matching set of
+criteria (barring the default_cols mapping) will be applied to the row, and no others.
+If False, the criteria will be applied in sequence to the row, and the last matching
+mapping will provide the output.
 """
 
 import logging
 import pandas as pd
 
 from transform_data.apply_datatypes import apply_datatypes
-from transform_data.table_transforms_annual_report_logs import TABLE_TRANSFORM_ANNUAL_REPORT_LOGS
+from transform_data.table_transforms_annual_report_logs import (
+    TABLE_TRANSFORM_ANNUAL_REPORT_LOGS,
+)
 from transform_data.table_transforms_annual_report_lodging_details import (
-    TABLE_TRANSFORM_ANNUAL_REPORT_LODGING_DETAILS
+    TABLE_TRANSFORM_ANNUAL_REPORT_LODGING_DETAILS,
 )
 from transform_data.table_transforms_cases import TABLE_TRANSFORM_CASES
 
-log = logging.getLogger('root')
+log = logging.getLogger("root")
 
 
 # definition of default set of transforms which may be applied to tables
@@ -76,9 +86,9 @@ log = logging.getLogger('root')
 # this maps from transform names (as used in mapping spreadsheets) to datatypes, mappings
 # and local variables to be applied, typically specified in imported modules
 DEFAULT_TRANSFORMS = {
-    'set_annual_report_logs_status': TABLE_TRANSFORM_ANNUAL_REPORT_LOGS,
-    'set_annual_report_lodging_details_status': TABLE_TRANSFORM_ANNUAL_REPORT_LODGING_DETAILS,
-    'set_cases_orderstatus': TABLE_TRANSFORM_CASES
+    "set_annual_report_logs_status": TABLE_TRANSFORM_ANNUAL_REPORT_LOGS,
+    "set_annual_report_lodging_details_status": TABLE_TRANSFORM_ANNUAL_REPORT_LODGING_DETAILS,
+    "set_cases_orderstatus": TABLE_TRANSFORM_CASES,
 }
 
 
@@ -87,7 +97,9 @@ class TableTransformException(Exception):
     pass
 
 
-def apply_table_transformation(df: pd.DataFrame, mapping: dict, local_vars: dict={}) -> pd.DataFrame:
+def apply_table_transformation(
+    df: pd.DataFrame, mapping: dict, local_vars: dict = {}, match_once: bool = False
+) -> pd.DataFrame:
     """
     Apply default columns from mapping, or check criteria in mapping and set
     output columns if criteria are met
@@ -97,43 +109,55 @@ def apply_table_transformation(df: pd.DataFrame, mapping: dict, local_vars: dict
         dataframe + output columns to set if criteria met
     :param local_vars: dict; local variables which will be interpolated into the
         criteria in the mappings
+    :param match_once: bool; if True, only the first matching mapping is applied; if False, the
+        last matching mapping will generate the output
     :return: dataframe
     """
-    if 'default_cols' in mapping:
-        for output_col, value in mapping['default_cols'].items():
+    if "default_cols" in mapping:
+        for output_col, value in mapping["default_cols"].items():
             df[output_col] = value
 
-    elif 'criteria' in mapping:
+    elif "criteria" in mapping:
         parsed = []
-        for condition in mapping['criteria']:
+        for condition in mapping["criteria"]:
             if isinstance(condition, str):
                 parsed.append(condition)
 
             elif isinstance(condition, dict):
-                if 'all_unset' in condition:
+                if "all_unset" in condition:
                     # all_unset() returns True if all columns have null, NaT or "" values
                     parsed += list(
-                        map(lambda field: f'({field}.isnull() | {field} == "")', condition['all_unset'])
+                        map(
+                            lambda field: f'({field}.isnull() | {field} == "")',
+                            condition["all_unset"],
+                        )
                     )
 
-                elif 'any_set' in condition:
+                elif "any_set" in condition:
                     # OR clause which checks for at least one column which is not null and not ""
-                    clauses = map(lambda field: f'({field}.notnull() & {field} != "")', condition['any_set'])
-                    parsed.append('((' + ') | ('.join(clauses) + '))')
+                    clauses = map(
+                        lambda field: f'({field}.notnull() & {field} != "")',
+                        condition["any_set"],
+                    )
+                    parsed.append("((" + ") | (".join(clauses) + "))")
 
-        query = '(' + ') & ('.join(parsed) + ')'
+        query = "(" + ") & (".join(parsed) + ")"
 
         # find matching rows and set column values
-        output_cols = mapping['output_cols']
-        log.debug(f'Table transform query: {query} => {output_cols}')
+        output_cols = mapping["output_cols"]
+        log.debug(f"Table transform query: {query} => {output_cols}")
 
         try:
             # cast to list is required as values() returns a dict view which is not treated as expected
             # by pandas
-            df.loc[df.eval(query, local_dict=local_vars), list(output_cols)] = list(output_cols.values())
+            df.loc[df.eval(query, local_dict=local_vars), list(output_cols)] = list(
+                output_cols.values()
+            )
         except pd.core.computation.ops.UndefinedVariableError as e:
-            log.error('ERROR creating query for table transform; likely missing local_vars key')
-            raise(e)
+            log.error(
+                "ERROR creating query for table transform; likely missing local_vars key"
+            )
+            raise (e)
 
     return df
 
@@ -141,7 +165,7 @@ def apply_table_transformation(df: pd.DataFrame, mapping: dict, local_vars: dict
 def process_table_transformations(
     df: pd.DataFrame,
     transforms_for_table: dict,
-    transform_definitions: dict=DEFAULT_TRANSFORMS
+    transform_definitions: dict = DEFAULT_TRANSFORMS,
 ) -> pd.DataFrame:
     """
     Apply table-level transforms to a dataframe
@@ -173,51 +197,57 @@ def process_table_transformations(
     for transform_name, params in transforms_for_table.items():
         transform = transform_definitions.get(transform_name)
         if transform is None:
-            raise TableTransformException(f'No table transform named {transform_name} exists')
+            raise TableTransformException(
+                f"No table transform named {transform_name} exists"
+            )
 
         # make the "c_" column names from the source columns specified in the transform config
-        source_cols = set(map(
-            lambda col_name: 'c_' + col_name.replace(' ', '_').lower(), params['source_cols']
-        ))
+        source_cols = set(
+            map(
+                lambda col_name: "c_" + col_name.replace(" ", "_").lower(),
+                params["source_cols"],
+            )
+        )
 
         # transform should only set columns in this Set
-        target_cols = set(params['target_cols'])
+        target_cols = set(params["target_cols"])
 
         # validate that the source_cols are all present in df
         missing_cols = source_cols - set(df.columns)
         if len(missing_cols) > 0:
             raise TableTransformException(
-                f'Dataframe is missing source columns required by {transform_name} '
-                f'table transform: {sorted(missing_cols)}'
+                f"Dataframe is missing source columns required by {transform_name} "
+                f"table transform: {sorted(missing_cols)}"
             )
 
         # apply datatypes to df (if specified); datetimes which fail to parse
         # are coerced to NaT
-        if 'datatypes' in transform:
-            df = apply_datatypes(transform['datatypes'], df, datetime_errors='coerce')
+        if "datatypes" in transform:
+            df = apply_datatypes(transform["datatypes"], df, datetime_errors="coerce")
 
         # call transformation function with mappings
-        log.debug(f'Applying table transform {transform_name}')
+        log.debug(f"Applying table transform {transform_name}")
 
-        mappings = transform['mappings']
-        local_vars = transform['local_vars']
+        mappings = transform["mappings"]
+        local_vars = transform.get("local_vars", {})
+        match_once = transform.get("match_once", False)
 
         for mapping in mappings:
             # validate that default_cols/output_cols in the mapping are all in target_cols
-            if 'default_cols' in mapping:
-                output_cols = mapping['default_cols']
+            if "default_cols" in mapping:
+                output_cols = mapping["default_cols"]
             else:
-                output_cols = mapping['output_cols']
+                output_cols = mapping["output_cols"]
 
             invalid_output_cols = set(output_cols.keys()) - target_cols
 
             if len(invalid_output_cols) > 0:
                 raise TableTransformException(
-                    f'Mapping for table transform {transform_name} outputs to column(s) '
-                    f'{sorted(invalid_output_cols)} which are not specified '
-                    f'in target_cols list {sorted(target_cols)}'
+                    f"Mapping for table transform {transform_name} outputs to column(s) "
+                    f"{sorted(invalid_output_cols)} which are not specified "
+                    f"in target_cols list {sorted(target_cols)}"
                 )
 
-            df = apply_table_transformation(df, mapping, local_vars)
+            df = apply_table_transformation(df, mapping, local_vars, match_once)
 
     return df
