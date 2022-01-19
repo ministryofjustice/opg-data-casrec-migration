74ae7090f3690c7226a8d9b5cbb0e222e0ba41d2
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-01-19 09:18:42 +0000

    Set client_id on pending annual_report_logs

--- migration_steps/transform_casrec/transform/app/entities/reporting/annual_report_logs_pending.py
+++ migration_steps/transform_casrec/transform/app/entities/reporting/annual_report_logs_pending.py
@@ -7,10 +7,10 @@ table here, we rely on the function in annual_report_logs.py to
 have done that for us by the time this is called. This is because
 the table creation function depends on us having a full table
 dataframe available to generate the CREATE TABLE SQL. We don't
-have that here, so we can't rely on the create_empty_table() method
-to set up the table properly.
+have that here (as we only have a partial record and no
+full mapping from pat -> annual_report_logs), so we can't
+rely on the create_empty_table() method to set up the table properly.
 """
-from custom_errors import EmptyDataFrame
 from utilities.basic_data_table import get_basic_data_table
 import logging
 import os
@@ -18,9 +18,8 @@ import pandas as pd
 
 from helpers import get_mapping_dict, get_table_def
 from transform_data.apply_datatypes import reapply_datatypes_to_fk_cols
+from transform_data.unique_id import add_unique_id
 from utilities.standard_transformations import calculate_duedate
-
-# from transform_data.unique_id import add_unique_id
 
 log = logging.getLogger("root")
 
@@ -30,7 +29,6 @@ def insert_annual_report_logs_pending(db_config, target_db, mapping_file):
     offset = -chunk_size
     chunk_no = 0
 
-    """
     persons_query = (
         f"select p.id as client_id, p.caserecnumber as caserecnumber "
         f'from {db_config["target_schema"]}.persons as p '
@@ -38,7 +36,6 @@ def insert_annual_report_logs_pending(db_config, target_db, mapping_file):
     )
 
     persons_df = pd.read_sql_query(persons_query, db_config["db_connection_string"])
-    """
 
     mapping_file_name = f"{mapping_file}_mapping"
     table_definition = get_table_def(mapping_name=mapping_file)
@@ -74,27 +71,6 @@ def insert_annual_report_logs_pending(db_config, target_db, mapping_file):
         2. Add 1 day to this end date and use value to set reportingperiodstartdate
         """
 
-        """
-        # set client_id
-        annual_report_log_df = annual_report_log_df.merge(
-            persons_df,
-            how="left",
-            left_on="c_case",
-            right_on="caserecnumber",
-        )
-
-        # as we've done a join, id may be duplicated within the dataframe,
-        # so adjust it
-        annual_report_log_df = annual_report_log_df.drop(columns=["id"])
-
-        annual_report_log_df = add_unique_id(
-            db_conn_string=db_config["db_connection_string"],
-            db_schema=db_config["target_schema"],
-            table_definition=table_definition,
-            source_data_df=annual_report_log_df,
-        )
-        """
-
         num_rows = len(annual_report_log_df)
 
         # NB we don't try to create the table if there are
@@ -107,6 +83,28 @@ def insert_annual_report_logs_pending(db_config, target_db, mapping_file):
             log.info(f"No data returned from database")
             break
 
+        log.debug(
+            f"Found {num_rows} rows in pat table to potentially insert into annual_report_logs"
+        )
+
+        # set client_id; inner join used, so if a client doesn't exist
+        # for the case attached to the pat record, the report will be removed
+        annual_report_log_df = annual_report_log_df.merge(
+            persons_df,
+            how="inner",
+            left_on="c_case",
+            right_on="caserecnumber",
+        )
+
+        num_rows = len(annual_report_log_df)
+        log.debug(f"After joining pat to persons, {num_rows} rows remain")
+
+        if num_rows == 0:
+            log.info(
+                "No rows remain to insert after join to persons; going to next chunk"
+            )
+            continue
+
         # calculate the duedate
         annual_report_log_df = calculate_duedate(
             original_col="reportingperiodenddate",
@@ -114,12 +112,19 @@ def insert_annual_report_logs_pending(db_config, target_db, mapping_file):
             df=annual_report_log_df,
         )
 
-        # client_id and order_id are NULL for now
-        annual_report_log_df["client_id"] = None
+        # order_id is NULL for now
         annual_report_log_df["order_id"] = None
 
         annual_report_log_df = reapply_datatypes_to_fk_cols(
             columns=["client_id", "order_id"], df=annual_report_log_df
+        )
+
+        # set an ID on all the new records
+        annual_report_log_df = add_unique_id(
+            db_conn_string=db_config["db_connection_string"],
+            db_schema=db_config["target_schema"],
+            table_definition=table_definition,
+            source_data_df=annual_report_log_df,
         )
 
         target_db.insert_data(
