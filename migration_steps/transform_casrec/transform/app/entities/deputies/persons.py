18068d78aedd91ca135e0caa705f212f556348a7
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-01-26 07:10:57 +0000

    Transform columns dependent on deputy being PA or Lay

--- migration_steps/transform_casrec/transform/app/entities/deputies/persons.py
+++ migration_steps/transform_casrec/transform/app/entities/deputies/persons.py
@@ -1,11 +1,36 @@
 import logging
+import pandas as pd
 
 from utilities.basic_data_table import get_basic_data_table
 
 from custom_errors import EmptyDataFrame
 from helpers import get_mapping_dict, get_table_def
+from transform_data.apply_datatypes import apply_datatypes
 
 log = logging.getLogger("root")
+
+
+def _set_columns_for_deputy_type(row: pd.Series) -> pd.Series:
+    row["organisationname"] = None
+    row["deputysubtype"] = None
+
+    if row["deputytype"] == "PUBLICAUTHORITY":
+        row["deputysubtype"] = "ORGANISATION"
+
+        # Clear name columns for public authorities
+
+        # Ordering is important for next two lines
+        row["organisationname"] = row["surname"]
+        row["surname"] = None
+
+        row["salutation"] = None
+        row["firstname"] = None
+        row["middlenames"] = None
+
+    elif row["deputytype"] == "LAY":
+        row["deputysubtype"] = "PERSON"
+
+    return row
 
 
 def insert_persons_deputies(db_config, target_db, mapping_file):
@@ -33,6 +58,15 @@ def insert_persons_deputies(db_config, target_db, mapping_file):
                 sirius_details=sirius_details,
                 chunk_details={"chunk_size": chunk_size, "offset": offset},
             )
+
+            # This has to be done here, as it fails if set
+            # in the mapping spreadsheet and automated during get_basic_data_table()
+            persons_df["deputycasrecid"] = persons_df["casrec_row_id"]
+
+            # Apply additional transforms manually which depend
+            # on whether this is a Lay or PA deputy (see IN-1118)
+            persons_df = persons_df.apply(_set_columns_for_deputy_type, axis=1)
+
             target_db.insert_data(
                 table_name=table_definition["destination_table_name"],
                 df=persons_df,
