00645482a40c7563301b1e5dbf44ff1fcaa5643e
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-02-08 14:01:31 +0000

    IN-1149 update tf ver and tidy up unused resources

--- terraform/shared/state_machine.tf
+++ terraform/shared/state_machine.tf
@@ -1,10 +1,94 @@
-data "aws_iam_role" "state_machine" {
-  name = "casrec-mig-state-machine.${local.environment}"
+data "aws_iam_role" "execution_role" {
+  name = "migration-execution-role.${local.environment}"
+}
+
+data "aws_iam_role" "etl" {
+  name = "casrec-migration-${local.environment}"
+}
+
+resource "aws_iam_role" "state_machine" {
+  assume_role_policy = data.aws_iam_policy_document.state_assume.json
+  name               = "casrec-mig-state-machine.${local.environment}"
+  tags               = local.default_tags
+}
+
+data "aws_iam_policy_document" "state_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["states.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "state_machine" {
+  statement {
+    effect = "Allow"
+    resources = [
+      data.aws_iam_role.etl.arn,
+      data.aws_iam_role.execution_role.arn
+    ]
+    actions = [
+      "iam:GetRole",
+      "iam:PassRole"
+    ]
+  }
+  statement {
+    effect = "Allow"
+    resources = [
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl0-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl1-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl2-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl3-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl4-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/etl5-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/initialise-environments-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/load-casrec-db-${terraform.workspace}*",
+      "arn:aws:ecs:eu-west-1:${local.account.account_id}:task-definition/reset-elasticsearch-${terraform.workspace}*"
+    ]
+    actions = ["ecs:RunTask"]
+  }
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "ecs:StopTask",
+      "ecs:DescribeTasks"
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "events:PutTargets",
+      "events:PutRule",
+      "events:DescribeRule"
+    ]
+  }
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "xray:PutTraceSegments",
+      "xray:PutTelemetryRecords",
+      "xray:GetSamplingRules",
+      "xray:GetSamplingTargets"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "state_machine" {
+  name   = "casrec-mig-state.${local.environment}"
+  policy = data.aws_iam_policy_document.state_machine.json
+  role   = aws_iam_role.state_machine.id
 }
 
 resource "aws_sfn_state_machine" "casrec_migration_load" {
   name       = "casrec-mig-load-state-machine-${local.account.name}"
-  role_arn   = data.aws_iam_role.state_machine.arn
+  role_arn   = aws_iam_role.state_machine.arn
   definition = local.load_casrec_definition
 }
 
