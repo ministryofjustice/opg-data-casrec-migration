e28428799a574094f51ab7a752a0557331c2ba95
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-10 08:24:52 +0000

    IN-1095 Set tasks due date to casrec.sup_activity Target or Start Date (#560)
    * Add unit tests for coalesce()
    
    Mirrors the SQL COALESCE function, setting a column to the first
    non-empty value in a list of columns.
    
    * Implement coalesce() standard transformation
    
    * Use coalesce() transform to set tasks duedate
    
    Use casrec sup_activity ActStart or Start Date for duedate on Sirius task.
    
    * Don't use simple transform for tasks duedate
    
    It's not possible to use a column from the casrec source table
    as both the source column for a simple mapping and as an input
    to a simple transform. This is because do_simple_mapping()
    renames the columns, rather than adding a new column with the
    target name. Consequently, if you attempt to refer to a column
    used in a simple mapping from the specification for a simple
    transform, the column no longer exists and it fails.
    
    * Remove duplicated code
    
    Lots of the lines in this file are redundant, so do some
    reflection to instead call the transform functions by name
    as required.
    
    * Manually use coalesce() in the tasks transform function
    
    As we can't apply the coalesce() function as a simple transform,
    manually apply it to the dataframe in the tasks transform.
    
    * Better handling of null column values
    
    Rather than only checking for None and "", use pandas.notnull()
    and "" instead. This ensures that values like pandas.NaT and
    numpy.nan (as well as None) are recognised correctly.
    
    * Fix tasks validation
    
    * Merge changes to spreadsheet after rebase
    
    Although the changes to the mapping JSON could be merged
    automatically, spreadsheet has to be manually updated so
    that it corresponds.

---
+++ migration_steps/transform_casrec/transform/transform_tests/utilities_tests/standard_transformations/coalesce/test_coalesce.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+from utilities.standard_transformations import coalesce
+
+
+def test_coalesce():
+    test_data = {
+        "id": [
+            1,  # 1st source column "", 2nd None
+            2,  # 1st source column None, 2nd ""
+            3,  # 1st source column has value, 2nd None
+            4,  # 1st source column None, 2nd has value
+            5,  # both columns have a value (first should be used)
+            6,  # 1st source column has a value, target column has a value and should be overridden
+            7,  # 2nd source column has a value, target column has a value and should be overridden,
+            8,  # 1st source column is NaT, 2nd has a value
+        ],
+        "Start Date": [
+            "",
+            None,
+            "2022-02-07",
+            None,
+            "2022-02-09",
+            "2022-02-11",
+            None,
+            pd.NaT,
+        ],
+        "Actdate": [
+            None,
+            "",
+            None,
+            "2022-02-08",
+            "2022-02-10",
+            None,
+            "2022-02-12",
+            "2022-02-15",
+        ],
+        "duedate": [
+            None,
+            None,
+            None,
+            None,
+            None,
+            "2022-02-13",
+            "2022-02-14",
+            None,
+        ],
+    }
+    test_data_df = pd.DataFrame(test_data)
+
+    result_col = "duedate"
+
+    expected_data = test_data.copy()
+    expected_data[result_col] = [
+        None,
+        None,
+        "2022-02-07",
+        "2022-02-08",
+        "2022-02-09",
+        "2022-02-11",
+        "2022-02-12",
+        "2022-02-15",
+    ]
+    expected_data_df = pd.DataFrame(expected_data)
+
+    result_df = coalesce(
+        original_cols=[
+            "Start Date",
+            "Actdate",
+        ],
+        result_col=result_col,
+        df=test_data_df,
+    )
+
+    assert_frame_equal(expected_data_df, result_df)
