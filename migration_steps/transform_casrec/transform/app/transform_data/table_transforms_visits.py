861fef9c0df7b9558ebd51047911c81800e20a61
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-11 16:51:01 +0000

    Complete implementation of visitsubtype rules

--- migration_steps/transform_casrec/transform/app/transform_data/table_transforms_visits.py
+++ migration_steps/transform_casrec/transform/app/transform_data/table_transforms_visits.py
@@ -12,6 +12,41 @@ _mappings = [
         ],
         "output_cols": {"visitsubtype": "VST-LAY"},
     },
+    {
+        "criteria": [
+            "c_req_by.isin([1, 2, 4, 5, 6, 7])",
+            "c_report_type == 2",
+        ],
+        "output_cols": {"visitsubtype": "VST-HW"},
+    },
+    {
+        "criteria": [
+            "c_req_by.isin([1, 2, 4, 5, 6, 7])",
+            "c_report_type == 3",
+        ],
+        "output_cols": {"visitsubtype": "VST-MED"},
+    },
+    {
+        "criteria": [
+            "c_req_by == 3",
+            "c_report_type.isin([1, 2])",
+        ],
+        "output_cols": {"visitsubtype": "VST-DEP"},
+    },
+    {
+        "criteria": [
+            "c_req_by == 3",
+            "c_report_type == 3",
+        ],
+        "output_cols": {"visitsubtype": "VST-MLPA"},
+    },
+    {
+        "criteria": [
+            "c_req_by.isin([8, 9, 10, 11, 12, 13, 14])",
+            "c_report_type.isin([1, 2, 3])",
+        ],
+        "output_cols": {"visitsubtype": "VST-PRO"},
+    },
 ]
 
 
