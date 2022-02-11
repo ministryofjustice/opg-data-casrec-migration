42b0d8a6a96c428679e4827e036dcbbce447b2c7
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-11 16:51:01 +0000

    Complete implementation of visitsubtype rules

--- migration_steps/transform_casrec/transform/transform_tests/transform_data_tests/table_transforms/test_table_transforms_visits.py
+++ migration_steps/transform_casrec/transform/transform_tests/transform_data_tests/table_transforms/test_table_transforms_visits.py
@@ -21,6 +21,112 @@ def case_table_transforms_set_visits_visitsubtype_vstlay():
     return (test_data, expected_data)
 
 
+def case_table_transforms_set_visits_visitsubtype_vsthw():
+    test_data = {
+        "c_req_by": [
+            "1",
+            "2",
+            "4",
+            "5",
+            "6",
+            "7",
+        ],
+        "c_report_type": list("2" * 6),
+    }
+    expected_data = {"visitsubtype": "VST-HW"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstmed():
+    test_data = {
+        "c_req_by": [
+            "1",
+            "2",
+            "4",
+            "5",
+            "6",
+            "7",
+        ],
+        "c_report_type": list("3" * 6),
+    }
+    expected_data = {"visitsubtype": "VST-MED"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstdep():
+    test_data = {
+        "c_req_by": [
+            "3",
+            "3",
+        ],
+        "c_report_type": ["1", "2"],
+    }
+    expected_data = {"visitsubtype": "VST-DEP"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstmlpa():
+    test_data = {
+        "c_req_by": [
+            "3",
+        ],
+        "c_report_type": ["3"],
+    }
+    expected_data = {"visitsubtype": "VST-MLPA"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstpro1():
+    test_data = {
+        "c_req_by": [
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+        ],
+        "c_report_type": list("1" * 7),
+    }
+    expected_data = {"visitsubtype": "VST-PRO"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstpro2():
+    test_data = {
+        "c_req_by": [
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+        ],
+        "c_report_type": list("2" * 7),
+    }
+    expected_data = {"visitsubtype": "VST-PRO"}
+    return (test_data, expected_data)
+
+
+def case_table_transforms_set_visits_visitsubtype_vstpro3():
+    test_data = {
+        "c_req_by": [
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+        ],
+        "c_report_type": list("3" * 7),
+    }
+    expected_data = {"visitsubtype": "VST-PRO"}
+    return (test_data, expected_data)
+
+
 @parametrize_with_cases(
     "test_data, expected_data",
     cases=".",
