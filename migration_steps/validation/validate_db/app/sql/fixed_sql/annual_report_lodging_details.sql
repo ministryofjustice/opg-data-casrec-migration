03414ab582feda570d08b5787a0c3c3eb610a448
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-17 17:40:15 +0000

    Validate correct tables (annual_report_lodging_details)

--- migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_lodging_details.sql
+++ migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_lodging_details.sql
@@ -4,20 +4,19 @@ DROP TABLE IF EXISTS casrec_csv.exceptions_annual_report_lodging_details;
 
 CREATE TABLE casrec_csv.exceptions_annual_report_lodging_details(
     caserecnumber text default NULL,
-    deadlinedate text default NULL,
     datereportlodged text default NULL,
-    bankstatementdeadlinedate text default NULL,
     bankstatementsreceived text default NULL,
     lodgedstatus text default NULL,
-    lodgedby_id text default NULL
+    lodgedby_id text default NULL,
+    bankstatementdeadlinedate date default NULL,
+    deadlinedate date default NULL,
+    resubmitteddate date default NULL
 );
 
 INSERT INTO casrec_csv.exceptions_annual_report_lodging_details(
     SELECT
         caserecnumber as caserecnumber,
-        CAST(NULLIF(follow_up_date, '') AS date) AS deadlinedate,
         CAST(NULLIF(datereportlodged, '') AS date) AS datereportlodged,
-        sent_date AS bankstatementdeadlinedate,
         CASE
         	WHEN bankstatementsreceived = 'Y' THEN TRUE
         	ELSE FALSE
@@ -36,7 +35,10 @@ INSERT INTO casrec_csv.exceptions_annual_report_lodging_details(
 	            ), 2
 	        )
         ), '') AS lodgedstatus,
-        2657 as lodgedby_id
+        2657 as lodgedby_id,
+        casrec_csv.report_bankstatementdeadlinedate(latest_further_date, last_further_code) AS bankstatementdeadlinedate,
+        casrec_csv.report_deadlinedate(latest_further_date, last_further_code) AS deadlinedate,
+        casrec_csv.report_resubmitteddate(further_codes, rcvd_dates) AS resubmitteddate
     FROM (
         SELECT
             "Case" AS caserecnumber,
@@ -69,18 +71,72 @@ INSERT INTO casrec_csv.exceptions_annual_report_lodging_details(
             "Lodge Date" AS lodged_date,
             "Review Date" AS reviewdate,
             "Next Yr" AS next_yr_flag,
-            "Further Code" AS further_code
+            "Further Code" AS further_code,
+
+            -- get latest date from "Further Date*" columns
+            GREATEST(
+                CAST(NULLIF("Further Date1", '') AS date),
+                CAST(NULLIF("Further Date2", '') AS date),
+                CAST(NULLIF("Further Date3", '') AS date),
+                CAST(NULLIF("Further Date4", '') AS date),
+                CAST(NULLIF("Further Date6", '') AS date),
+                CAST(NULLIF("Further Date6.1", '') AS date)
+            ) AS latest_further_date,
+
+            -- in alphabetical order (Further1 is on left, Further6 on the right), get the last
+            -- value which is not 0 or 2
+            CAST(
+                TO_JSON(
+                    ARRAY_REMOVE(
+                        ARRAY_REMOVE(
+                            ARRAY[
+                                CAST(NULLIF("Further1", '') AS text),
+                                CAST(NULLIF("Further2", '') AS text),
+                                CAST(NULLIF("Further3", '') AS text),
+                                CAST(NULLIF("Further4", '') AS text),
+                                CAST(NULLIF("Further5", '') AS text),
+                                CAST(NULLIF("Further6", '') AS text)
+                            ],
+                            '0'
+                        ),
+                        '2'
+                    )
+                )->>-1
+            AS text) AS last_further_code,
+
+            -- all "Further*" column values
+            ARRAY[
+                CAST(NULLIF("Further1", '') AS text),
+                CAST(NULLIF("Further2", '') AS text),
+                CAST(NULLIF("Further3", '') AS text),
+                CAST(NULLIF("Further4", '') AS text),
+                CAST(NULLIF("Further5", '') AS text),
+                CAST(NULLIF("Further6", '') AS text)
+            ] AS further_codes,
+
+            -- all "Rcvd Date*" column values
+            ARRAY[
+                CAST(NULLIF("Rcvd Date1", '') AS date),
+                CAST(NULLIF("Rcvd Date2", '') AS date),
+                CAST(NULLIF("Rcvd Date3", '') AS date),
+                CAST(NULLIF("Rcvd Date4", '') AS date),
+                CAST(NULLIF("Rcvd Date5", '') AS date),
+                CAST(NULLIF("Rcvd Date6", '') AS date)
+            ] AS rcvd_dates
         FROM casrec_csv.account
     ) AS lodge_details
+
     EXCEPT
+
     SELECT
         persons.caserecnumber AS caserecnumber,
-        CAST(annual_report_lodging_details.deadlinedate AS date) AS deadlinedate,
         CAST(annual_report_lodging_details.datereportlodged AS date) AS datereportlodged,
-        CAST(annual_report_lodging_details.bankstatementdeadlinedate AS date) AS bankstatementdeadlinedate,
         annual_report_lodging_details.bankstatementsreceived AS bankstatementsreceived,
         annual_report_lodging_details.lodgedstatus AS lodgedstatus,
-        annual_report_lodging_details.lodgedby_id AS lodgedby_id
+        annual_report_lodging_details.lodgedby_id AS lodgedby_id,
+        annual_report_lodging_details.bankstatementdeadlinedate,
+        annual_report_lodging_details.deadlinedate,
+        annual_report_lodging_details.resubmitteddate
     FROM {target_schema}.annual_report_lodging_details
     INNER JOIN {target_schema}.annual_report_logs on annual_report_logs.id = annual_report_lodging_details.annual_report_log_id
     INNER JOIN {target_schema}.persons on persons.id = annual_report_logs.client_id
