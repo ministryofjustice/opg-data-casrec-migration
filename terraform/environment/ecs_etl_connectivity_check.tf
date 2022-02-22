41ce5692452a8b8d0e27449c647405d6869c8863
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2022-02-22 19:32:59 +0000

    In 1166 - Check connectivity on environment without doing anything (#594)

---
+++ terraform/environment/ecs_etl_connectivity_check.tf
@@ -0,0 +1,114 @@
+resource "aws_ecs_task_definition" "etl_connectivity" {
+  family                   = "etl-connectivity-${terraform.workspace}"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 2048
+  memory                   = 4096
+  container_definitions    = "[${local.etl_connectivity}]"
+  task_role_arn            = aws_iam_role.etl.arn
+  execution_role_arn       = aws_iam_role.execution_role.arn
+  tags = merge(local.default_tags,
+    { "Role" = "casrec-migration-${local.environment}" },
+  )
+}
+
+locals {
+  etl_connectivity = jsonencode({
+    cpu       = 0,
+    essential = true,
+    image     = local.images.etl0,
+    name      = "etl0",
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.casrec_migration.name,
+        awslogs-region        = "eu-west-1",
+        awslogs-stream-prefix = "casrec-migration-etl-connectivity-${local.environment}"
+      }
+    },
+    secrets = [
+      {
+        name      = "DB_PASSWORD",
+        valueFrom = aws_secretsmanager_secret.casrec_migration.arn
+      },
+      {
+        name      = "SIRIUS_DB_PASSWORD"
+        valueFrom = local.sirius_db_password
+      },
+      {
+        name      = "CASREC_DB_PASSWORD",
+        valueFrom = data.aws_secretsmanager_secret.casrec_migration_load.arn
+      },
+    ],
+    environment = [
+      {
+        name  = "DB_HOST",
+        value = aws_rds_cluster.cluster_serverless.endpoint
+      },
+      {
+        name  = "DB_PORT",
+        value = tostring(aws_rds_cluster.cluster_serverless.port)
+      },
+      {
+        name  = "DB_NAME",
+        value = aws_rds_cluster.cluster_serverless.database_name
+      },
+      {
+        name  = "DB_USER",
+        value = aws_rds_cluster.cluster_serverless.master_username
+      },
+      {
+        name  = "CASREC_DB_HOST",
+        value = data.aws_rds_cluster.casrec_load.endpoint
+      },
+      {
+        name  = "CASREC_DB_PORT",
+        value = tostring(data.aws_rds_cluster.casrec_load.port)
+      },
+      {
+        name  = "CASREC_DB_NAME",
+        value = data.aws_rds_cluster.casrec_load.database_name
+      },
+      {
+        name  = "CASREC_DB_USER",
+        value = data.aws_rds_cluster.casrec_load.master_username
+      },
+      {
+        name  = "SIRIUS_DB_HOST",
+        value = local.sirius_db_endpoint
+      },
+      {
+        name  = "SIRIUS_DB_PORT",
+        value = tostring(data.aws_rds_cluster.sirius.port)
+      },
+      {
+        name  = "SIRIUS_DB_NAME",
+        value = data.aws_rds_cluster.sirius.database_name
+      },
+      {
+        name  = "SIRIUS_DB_USER",
+        value = data.aws_rds_cluster.sirius.master_username
+      },
+      {
+        name  = "ACCOUNT_NAME",
+        value = local.account.account_name
+      },
+      {
+        name  = "ENVIRONMENT",
+        value = terraform.workspace
+      },
+      {
+        name  = "DEFAULT_CHUNK_SIZE",
+        value = local.account.default_chunk_size
+      },
+      {
+        name  = "SIRIUS_ACCOUNT",
+        value = local.account.account_id
+      },
+      {
+        name  = "SIRIUS_FRONT_URL",
+        value = "http://frontend.${local.account.sirius_env}.ecs"
+      },
+    ]
+  })
+}
