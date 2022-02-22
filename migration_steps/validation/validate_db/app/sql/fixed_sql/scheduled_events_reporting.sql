44a64c7a4a3c62f01bb46b6947589a14489a32a8
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-22 08:53:21 +0000

    Simplify validation SQL for scheduled events (reporting)

--- migration_steps/validation/validate_db/app/sql/fixed_sql/scheduled_events_reporting.sql
+++ migration_steps/validation/validate_db/app/sql/fixed_sql/scheduled_events_reporting.sql
@@ -14,43 +14,39 @@ CREATE INDEX ix_scheduled_events_json_clientid ON {target_schema}.scheduled_even
 CREATE INDEX ix_scheduled_events_json_class ON {target_schema}.scheduled_events USING BTREE ((event->>'class'));
 
 INSERT INTO casrec_csv.exceptions_scheduled_events_reporting(
-	SELECT * FROM (
-        SELECT
-            persons.caserecnumber AS caserecnumber,
-            annual_report_logs.id AS report_log_id,
-            1 AS version,
-            FALSE AS processed,
-            annual_report_logs.reportingperiodenddate + INTERVAL '1 YEAR - 1 DAY' AS dueby,
-            to_char(
-                annual_report_logs.reportingperiodenddate + INTERVAL '1 YEAR',
-                'YYYY-MM-DD"T"HH24:MI:SS+00:00'
-            ) AS enddate
-        FROM {target_schema}.annual_report_logs
-	    LEFT JOIN {target_schema}.persons ON persons.id = annual_report_logs.client_id
-	    WHERE persons.clientsource = 'CASRECMIGRATION'
-            AND annual_report_logs.status = 'PENDING'
-    ) AS sirius_reports_data
+    SELECT
+        persons.caserecnumber AS caserecnumber,
+        annual_report_logs.id AS report_log_id,
+        1 AS version,
+        FALSE AS processed,
+        annual_report_logs.reportingperiodenddate + INTERVAL '1 YEAR - 1 DAY' AS dueby,
+        to_char(
+            annual_report_logs.reportingperiodenddate + INTERVAL '1 YEAR',
+            'YYYY-MM-DD"T"HH24:MI:SS+00:00'
+        ) AS enddate
+    FROM {target_schema}.annual_report_logs
+    INNER JOIN {target_schema}.persons ON persons.id = annual_report_logs.client_id
+    WHERE persons.clientsource = 'CASRECMIGRATION'
+        AND annual_report_logs.status = 'PENDING'
 
     EXCEPT
 
-    SELECT * FROM (
-        SELECT
-            persons.caserecnumber AS caserecnumber,
-            annual_report_logs.id AS report_log_id,
-            scheduled_events.version AS version,
-            scheduled_events.processed AS processed,
-            scheduled_events.dueby AS dueby,
-            scheduled_events.event->'payload'->>'endDate' AS enddate
-        FROM
-            {target_schema}.annual_report_logs
-        LEFT JOIN {target_schema}.persons ON persons.id = annual_report_logs.client_id
-        LEFT JOIN {target_schema}.scheduled_events
-            ON scheduled_events.event->>'class' = 'Opg\Core\Model\Event\DeputyshipReporting\ScheduledReportingPeriodEndDate'
-            AND scheduled_events.event->'payload'->>'clientId' = persons.id::text
-            AND scheduled_events.event->'payload'->>'reportingPeriodId' = annual_report_logs.id::text
-        WHERE persons.clientsource = 'CASRECMIGRATION'
-            AND annual_report_logs.status = 'PENDING'
-    ) AS sirius_events_data
+    SELECT
+        persons.caserecnumber AS caserecnumber,
+        annual_report_logs.id AS report_log_id,
+        scheduled_events.version AS version,
+        scheduled_events.processed AS processed,
+        scheduled_events.dueby AS dueby,
+        scheduled_events.event->'payload'->>'endDate' AS enddate
+    FROM
+        {target_schema}.annual_report_logs
+    INNER JOIN {target_schema}.persons ON persons.id = annual_report_logs.client_id
+    INNER JOIN {target_schema}.scheduled_events
+        ON scheduled_events.event->>'class' = 'Opg\Core\Model\Event\DeputyshipReporting\ScheduledReportingPeriodEndDate'
+        AND scheduled_events.event->'payload'->>'clientId' = persons.id::text
+        AND scheduled_events.event->'payload'->>'reportingPeriodId' = annual_report_logs.id::text
+    WHERE persons.clientsource = 'CASRECMIGRATION'
+        AND annual_report_logs.status = 'PENDING'
 );
 
 DROP INDEX IF EXISTS ix_scheduled_events_json_payload;
