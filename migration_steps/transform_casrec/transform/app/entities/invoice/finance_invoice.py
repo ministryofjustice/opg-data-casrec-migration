d92d78f29672758deaeb2c3fb472b7c506c27178
devkokov <dm.kokov@gmail.com>
2022-02-23 17:23:17 +0000

    IN-1176 Set invoices to Confirmed if they are present in Aged Debt

--- migration_steps/transform_casrec/transform/app/entities/invoice/finance_invoice.py
+++ migration_steps/transform_casrec/transform/app/entities/invoice/finance_invoice.py
@@ -1,10 +1,11 @@
+import datetime
+
 import pandas as pd
 from custom_errors import EmptyDataFrame
 
 from transform_data.apply_datatypes import reapply_datatypes_to_fk_cols
 from utilities.basic_data_table import get_basic_data_table
 import logging
-import os
 
 from helpers import get_mapping_dict, get_table_def
 
@@ -26,6 +27,12 @@ def insert_finance_invoice(target_db, db_config, mapping_file):
     feecheck_query = f'select "Invoice Number", "GL Date" from {db_config["source_schema"]}.sop_feecheck;'
     feecheck_df = pd.read_sql_query(feecheck_query, db_config["db_connection_string"])
     feecheck_df = feecheck_df[["Invoice Number", "GL Date"]]
+
+    aged_debt_query = (
+        f'select "Trx Number" from {db_config["source_schema"]}.sop_aged_debt;'
+    )
+    aged_debt_df = pd.read_sql_query(aged_debt_query, db_config["db_connection_string"])
+    aged_debt_df = aged_debt_df[["Trx Number"]]
 
     mapping_file_name = f"{mapping_file}_mapping"
     table_definition = get_table_def(mapping_name=mapping_file)
@@ -71,6 +78,18 @@ def insert_finance_invoice(target_db, db_config, mapping_file):
             )
             invoice_joined_df = invoice_joined_df.drop(columns=["Invoice Number"])
 
+            # Join sop_aged_debt, so we can set a confirmeddate on all invoices present in Aged Debt
+            invoice_joined_df = invoice_joined_df.merge(
+                aged_debt_df,
+                how="left",
+                left_on="reference",
+                right_on="Trx Number",
+            )
+            invoice_joined_df["confirmeddate"] = invoice_joined_df.apply(
+                _confirmeddate, axis=1
+            )
+            invoice_joined_df = invoice_joined_df.drop(columns=["Trx Number"])
+
             invoice_joined_df = reapply_datatypes_to_fk_cols(
                 columns=["person_id"], df=invoice_joined_df
             )
@@ -89,3 +108,11 @@ def insert_finance_invoice(target_db, db_config, mapping_file):
                 )
                 break
             continue
+
+
+def _confirmeddate(row: pd.Series):
+    if not pd.isnull(row["confirmeddate"]):
+        return row["confirmeddate"]
+    if not pd.isnull(row["Trx Number"]):
+        return datetime.datetime.now().date()
+    return None
