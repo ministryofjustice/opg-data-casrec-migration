7680eeb8aad6cf053b40bb633305e1ef935dd5b6
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-02-23 17:08:18 +0000

    Fix validation SQL for report receiveddate

--- migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_logs.sql
+++ migration_steps/validation/validate_db/app/sql/fixed_sql/annual_report_logs.sql
@@ -21,12 +21,12 @@ INSERT INTO casrec_csv.exceptions_annual_report_logs(
             reportingperiodenddate,
             reportingperiodstartdate,
             duedate,
-            CAST(nullif(receiveddate, '') AS date) AS receiveddate,
-            CAST(nullif(revisedduedate, '') AS date) AS revisedduedate,
+            CAST(receiveddate AS date) AS receiveddate,
+            CAST(NULLIF(revisedduedate, '') AS date) AS revisedduedate,
             numberofchaseletters,
             casrec_csv.report_element(full_status, 1) AS status,
             NULLIF(casrec_csv.report_element(full_status, 3), '') AS reviewstatus,
-            CAST(nullif(reviewdate, '') AS date) AS reviewdate
+            CAST(NULLIF(reviewdate, '') AS date) AS reviewdate
         FROM (
             SELECT
                 caserecnumber,
@@ -48,14 +48,9 @@ INSERT INTO casrec_csv.exceptions_annual_report_logs(
                     CAST(account."End Date" AS date) AS reportingperiodenddate,
                     CAST(account."End Date" AS date) - INTERVAL '1 year' + INTERVAL '1 day' AS reportingperiodstartdate,
                     transf_calculate_duedate(account."End Date") AS duedate,
-                    GREATEST(
+                    COALESCE(
                         NULLIF(account."Rcvd Date", ''),
-                        NULLIF(account."Rcvd Date1", ''),
-                        NULLIF(account."Rcvd Date2", ''),
-                        NULLIF(account."Rcvd Date3", ''),
-                        NULLIF(account."Rcvd Date4", ''),
-                        NULLIF(account."Rcvd Date5", ''),
-                        NULLIF(account."Rcvd Date6", '')
+                        NULLIF(account."Lodge Date", '')
                     ) AS receiveddate,
                     "Revise Date" AS revisedduedate,
                     0 AS numberofchaseletters,
