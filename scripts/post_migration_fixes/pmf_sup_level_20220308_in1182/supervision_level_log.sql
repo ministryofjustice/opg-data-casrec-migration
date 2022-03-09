f66e52cd7c6b671f023a61f97c5ee09ce5eb7f28
devkokov <dm.kokov@gmail.com>
2022-03-09 20:19:29 +0000

    IN-1182 Keep supervision level logs added/updated since the Lay migration

--- scripts/post_migration_fixes/pmf_sup_level_20220308_in1182/supervision_level_log.sql
+++ scripts/post_migration_fixes/pmf_sup_level_20220308_in1182/supervision_level_log.sql
@@ -1,6 +1,8 @@
 CREATE SCHEMA IF NOT EXISTS pmf_sup_level_20220308_in1182;
 
 -- DROP TABLE IF EXISTS pmf_sup_level_20220308_in1182.supervision_level_log_audit;
+-- DROP TABLE IF EXISTS pmf_sup_level_20220308_in1182.supervision_level_log_keep;
+-- DROP TABLE IF EXISTS pmf_sup_level_20220308_in1182.supervision_level_log_deletes;
 -- DROP TABLE IF EXISTS pmf_sup_level_20220308_in1182.supervision_level_log_inserts;
 
 SET datestyle = "ISO, DMY";
@@ -11,6 +13,36 @@ FROM supervision_level_log sll
 INNER JOIN cases c ON c.id = sll.order_id
 INNER JOIN persons p ON p.id = c.client_id
 WHERE p.clientsource = 'CASRECMIGRATION';
+
+WITH migrated_sup_levels AS (
+    SELECT sll.*
+    FROM supervision_level_log sll
+    INNER JOIN cases c ON c.id = sll.order_id
+    INNER JOIN persons p ON p.id = c.client_id
+    WHERE p.clientsource = 'CASRECMIGRATION'
+)
+SELECT *
+INTO pmf_sup_level_20220308_in1182.supervision_level_log_keep
+FROM (
+    SELECT msl.*
+    FROM migrated_sup_levels msl
+    WHERE msl.appliesfrom <> '2022-03-05'::date
+       OR msl.createddate <> '2022-03-05'::timestamp(0)
+       OR msl.notes <> 'placeholder note'
+    UNION
+    SELECT id, order_id, appliesfrom, createddate, supervisionlevel, notes, assetlevel
+    FROM (
+        SELECT *, ROW_NUMBER() OVER (PARTITION BY order_id ORDER BY order_id, id) as row_number
+        FROM migrated_sup_levels
+    ) numbered_logs
+    WHERE numbered_logs.row_number <> 1
+) logs;
+
+SELECT *
+INTO pmf_sup_level_20220308_in1182.supervision_level_log_deletes
+FROM pmf_sup_level_20220308_in1182.supervision_level_log_audit audit
+LEFT JOIN pmf_sup_level_20220308_in1182.supervision_level_log_keep keep ON audit.id = keep.id
+WHERE keep.id IS NULL;
 
 WITH casrec_supervision_levels AS (
     SELECT a.*, ROW_NUMBER() OVER (ORDER BY a."Order No", a."Start Date"::date) AS row_number
@@ -43,8 +75,8 @@ ORDER BY csl1.row_number;
 
 BEGIN;
     DELETE FROM supervision_level_log sll
-    USING pmf_sup_level_20220308_in1182.supervision_level_log_audit slla
-    WHERE sll.id = slla.id;
+    USING pmf_sup_level_20220308_in1182.supervision_level_log_deletes del
+    WHERE sll.id = del.id;
 
     INSERT INTO supervision_level_log (id, order_id, appliesfrom, createddate, supervisionlevel, notes, assetlevel)
     SELECT nextval('supervision_level_log_id_seq'), slli.order_id, slli.appliesfrom, slli.createddate, slli.supervisionlevel, slli.notes, slli.assetlevel
@@ -76,4 +108,10 @@ SELECT 'not in Casrec' as validation, * FROM (
     SELECT * FROM sirius_side
     EXCEPT
     SELECT * FROM casrec_side
-) validation2;
+) validation2
+UNION
+SELECT 'erroneously deleted' as validation, * FROM (
+    SELECT order_id, appliesfrom, createddate, supervisionlevel, notes, assetlevel FROM pmf_sup_level_20220308_in1182.supervision_level_log_keep
+    EXCEPT
+    SELECT * FROM sirius_side
+) validation3;
