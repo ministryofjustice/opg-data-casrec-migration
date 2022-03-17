18f207abb47fa3b9d908efee72df6cfa449883eb
Elliot Smith <elliot.smith@digital.justice.gov.uk>
2022-03-17 11:59:55 +0000

    Modify to delete then insert annual_report_type_assignments

--- scripts/post_migration_fixes/pmf_report_type_assignments_20220308_in1172/annual_report_type_assignments_update.sql
+++ scripts/post_migration_fixes/pmf_report_type_assignments_20220308_in1172/annual_report_type_assignments_update.sql
@@ -8,92 +8,19 @@ CREATE SCHEMA IF NOT EXISTS pmf_report_type_assignments_20220308_in1172;
 -- record the current number of records in annual_report_type_assignments
 SELECT *
 INTO pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_counts
-FROM (SELECT COUNT(id) AS arta_starting_count FROM annual_report_type_assignments) artas;
+FROM (SELECT COUNT(1) AS arta_starting_count FROM annual_report_type_assignments) artas;
 
--- Populate table with data we're going to update
-SELECT *
-INTO pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates
-FROM (
-
-    SELECT
-        arta_id,
-        annualreport_id,
-        (CASE
-            WHEN supervisionlevel = 'GENERAL' THEN 'OPG102'
-            WHEN supervisionlevel = 'MINIMAL' THEN 'OPG103'
-            ELSE NULL
-        END) AS reporttype,
-        type
-    FROM (
-        SELECT
-            arta.id AS arta_id,
-            arl.id AS annualreport_id,
-            arl.casesupervisionlevel AS supervisionlevel,
-            c.casesubtype AS type
-        FROM annual_report_type_assignments arta
-
-        -- only include annual_report_type_assignments where
-        -- the reporttype and type have not changed since migration
-        INNER JOIN casrec_mapping.annual_report_type_assignments carta
-        ON arta.id = carta.sirius_id
-        AND carta.reporttype = arta.reporttype
-        AND carta.type = arta.type
-
-        -- only change annual_report_type_assignments for PENDING reports
-        INNER JOIN annual_report_logs arl
-        ON arta.annualreport_id = arl.id
-        AND arl.status = 'PENDING'
-
-        -- only update annual_report_type_assignments where the status
-        -- hasn't changed since the migration
-        INNER JOIN casrec_mapping.annual_report_logs carl
-        ON arl.id = carl.sirius_id
-        AND carl.status = arl.status
-
-        INNER JOIN cases c
-        ON arl.order_id = c.id
-    ) AS reports
-
-) to_update;
-
--- Audit table for updates
-SELECT *
-INTO pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates_audit
-FROM (
-    SELECT * FROM annual_report_type_assignments
-    WHERE id IN (
-        SELECT arta_id FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates
-    )
-) updates;
-
--- Delete the non-PENDING annual_report_type_assignments we incorrectly migrated
+-- Delete all of the annual_report_type_assignments we incorrectly migrated;
+-- we ignore whether they have been updated since the migration
 SELECT *
 INTO pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_deletes
 FROM (
 
-    SELECT arta.id AS arta_id, arl.client_id
+    SELECT arta.id AS arta_id
     FROM annual_report_type_assignments arta
 
-    -- only include annual_report_type_assignments where
-    -- the reporttype and type have not changed since migration
     INNER JOIN casrec_mapping.annual_report_type_assignments carta
     ON arta.id = carta.sirius_id
-    AND carta.reporttype = arta.reporttype
-    AND carta.type = arta.type
-
-    -- delete type assignments for non-PENDING reports only
-    INNER JOIN annual_report_logs arl
-    ON arta.annualreport_id = arl.id
-    AND (arl.status != 'PENDING' OR arl.status IS NULL)
-
-    INNER JOIN persons p
-    ON arl.client_id = p.id
-
-    -- only delete annual_report_type_assignments where the status
-    -- hasn't changed since the migration
-    INNER JOIN casrec_mapping.annual_report_logs carl
-    ON arl.id = carl.sirius_id
-    AND carl.status = arl.status
 
 ) to_delete;
 
@@ -107,37 +34,59 @@ FROM (
     )
 ) deletes;
 
-BEGIN;
-    UPDATE annual_report_type_assignments arta
-    SET type = up.type, reporttype = up.reporttype
-    FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates up
-    WHERE up.arta_id = arta.id AND up.annualreport_id = arta.annualreport_id;
+-- Populate table with data we're going to insert;
+-- no audit table for this, as the inserts table *is* the audit table
+SELECT *
+INTO pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_inserts
+FROM (
 
+    SELECT
+        nextval('annual_report_type_assignments_id_seq') AS id,
+        reports.id AS annualreport_id,
+        (CASE
+            WHEN reports.casesupervisionlevel = 'GENERAL' THEN 'OPG102'
+            WHEN reports.casesupervisionlevel = 'MINIMAL' THEN 'OPG103'
+            ELSE NULL
+        END) AS reporttype,
+        'pfa' AS type
+    FROM (
+        SELECT arl.id, arl.casesupervisionlevel, arl.status
+        FROM casrec_mapping.annual_report_logs carl
+        INNER JOIN annual_report_logs arl
+        ON carl.sirius_id = arl.id
+    ) reports
+    WHERE reports.status = 'PENDING'
+
+) to_insert;
+
+BEGIN;
     DELETE FROM annual_report_type_assignments
     WHERE id IN (
         SELECT arta_id FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_deletes
     );
 
-    -- Validation: check updates look OK
-    SELECT arta_id, annualreport_id, reporttype, type
-    FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates
-    EXCEPT
-    SELECT id, annualreport_id, reporttype, type
-    FROM annual_report_type_assignments
-    WHERE id IN (
-        SELECT id FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates_audit
-    );
+    INSERT INTO annual_report_type_assignments (id, annualreport_id, reporttype, type)
+    SELECT ins.id, ins.annualreport_id, ins.reporttype, ins.type
+    FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_inserts ins
+    -- only add a row if the arl row doesn't already have an arta row
+    LEFT JOIN annual_report_type_assignments arta
+    ON ins.annualreport_id = arta.annualreport_id
+    WHERE arta.id IS NULL;
 
-    -- Validation: check we haven't deleted too many rows, or updated too many
+    -- Validation
     SELECT
         *,
-        ("# arta start" = "# arta current" + "# arta deleted") AS "Deletes OK?"
+        ("# arta start" - "# arta deleted" + "# arta inserted" = "# arta current") AS "OK?"
     FROM (
         SELECT
             (SELECT arta_starting_count FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_counts) AS "# arta start",
-            (SELECT COUNT(arta.id) FROM annual_report_type_assignments arta) AS "# arta current",
-            (SELECT COUNT(arta_id) FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_deletes) AS "# arta deleted",
-            (SELECT COUNT(arta_id) FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_updates) AS "# arta updated"
+            (SELECT COUNT(1) FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_deletes) AS "# arta deleted",
+            (
+                SELECT COUNT(1) FROM pmf_report_type_assignments_20220308_in1172.annual_report_type_assignments_inserts ins
+                INNER JOIN annual_report_type_assignments arta
+                ON ins.id = arta.id
+            ) AS "# arta inserted",
+            (SELECT COUNT(1) FROM annual_report_type_assignments arta) AS "# arta current"
     ) counts;
 
 -- Manually run if validation incorrect
