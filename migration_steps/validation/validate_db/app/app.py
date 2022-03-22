48a8bd19b6ca88a84d2fde842725519cdbe4d705
Dimitar Kokov <dm.kokov@gmail.com>
2022-03-22 11:30:29 +0000

    Remove automated letters validation (#649)

--- migration_steps/validation/validate_db/app/app.py
+++ migration_steps/validation/validate_db/app/app.py
@@ -2,8 +2,6 @@ import sys
 import os
 from pathlib import Path
 
-from letters.sirius_letters import generate_letters
-
 current_path = Path(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, str(current_path) + "/../../../shared")
 
@@ -86,7 +84,6 @@ def get_mappings():
         "reporting": [
             "annual_report_logs",
             "annual_report_lodging_details",
-            "automated_letters",
         ],
         "invoice": ["finance_invoice_ad", "finance_invoice_non_ad"],
         "fee_reductions": ["finance_remissions", "finance_exemptions"],
@@ -868,9 +865,6 @@ def main(correfs, staging):
     if not is_staging:
         log.info("RUN SELECTIVE VACCUUM AND ANALYSE ON SIRIUS")
         vacuum_and_analyse()
-
-    log.info("SIMULATE AUTOMATED LETTERS")
-    generate_letters(conn_target, str(target_schema))
 
     log.info("RUN PRE VALIDATION")
     pre_validation()
