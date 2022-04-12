8ec51332ca89e3ab6d121e884c643d0ce2a96ca6
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-04-12 11:50:47 +0100

    IN-1245 remove further duplicates

---
+++ migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/19_pmf_reports_in1245/annual_report_logs.sql
@@ -0,0 +1,45 @@
+--Purpose: delete remaining pending reports above 1 that were missed in pmf5
+--@setup_tag
+CREATE SCHEMA IF NOT EXISTS {pmf_schema};
+
+SELECT count(*), ar.client_id
+INTO {pmf_schema}.affected_cases
+FROM annual_report_logs ar
+INNER JOIN persons p ON p.id = ar.client_id
+WHERE p.clientsource like 'CASRECMIGRATION%' --Have this apply universally
+AND ar.status = 'PENDING'
+GROUP BY ar.client_id
+HAVING count(*) > 1;
+
+SELECT id, reportingperiodenddate
+INTO {pmf_schema}.annual_report_logs_deletes
+FROM
+(
+    SELECT row_number() OVER (partition by ar.client_id order by ar.reportingperiodenddate asc, ar.id asc) as rown,
+    ar.id, ar.reportingperiodenddate
+    FROM annual_report_logs ar
+    INNER JOIN {pmf_schema}.affected_cases ac ON ac.client_id = ar.client_id
+    WHERE ar.status = 'PENDING'
+    ORDER BY ar.client_id
+) as a
+WHERE rown > 1;
+
+--@audit_tag
+SELECT ar.*
+INTO {pmf_schema}.reports_audit
+FROM {pmf_schema}.affected_cases ac
+INNER JOIN annual_report_logs ar ON ar.client_id = ac.client_id;
+
+--@update_tag
+DELETE FROM annual_report_logs ar
+USING {pmf_schema}.annual_report_logs_deletes rd
+WHERE ar.id = rd.id;
+
+--@validate_tag
+SELECT count(*), ar.client_id
+FROM annual_report_logs ar
+INNER JOIN persons p ON p.id = ar.client_id
+WHERE p.clientsource like 'CASRECMIGRATION%'
+AND ar.status = 'PENDING'
+GROUP BY ar.client_id
+HAVING count(*) > 1;
