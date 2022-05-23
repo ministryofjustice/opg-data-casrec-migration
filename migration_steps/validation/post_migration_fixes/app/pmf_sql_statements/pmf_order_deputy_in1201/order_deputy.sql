86ae2c8979fa8e317d5d1e952d1ae04ef46e9ce5
jackgoodby <jackgoodby@gmail.com>
2022-05-23 11:40:43 +0100

    IN1201 - Changes for SW-5461
    Sirius post-migration task SW-5461 created many duplicate deputies in order to save deputy addresses. Duplicates were marked as ‘Legacy’ and clients & orders were RE-MAPPED to new deputies.
    
    The impact of the above work on this ticket is that when we came to update ~2000 pro/pa rows in the order_deputy table, the rows were no longer there, because the orders are now being mapped to a replacement (de-duped) deputy.
    
    Working with Dimitar it has been possible to recreate the re-map from two files outputted by the remap script - seen in the sql as:
    - IN1201_split_deputies_final.csv
    - IN1201_new_deputies_final.csv
    
    From these files we create a lookup table ({pmf_schema}.changed_deputies) that we can use in the update script.
    
    The update table thus changes to make two joins to the order_deputy table, one using the original deputy id, and one using the updated deputy id, and coalesces the results)
    
    NOTE the remapping performed by Sirius fully removed the row from order_deputy - consequently there is no overlap between od and od2 in the SQL - where od is found, od2 is NULL, and vicaversa

--- migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/pmf_order_deputy_in1201/order_deputy.sql
+++ migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/pmf_order_deputy_in1201/order_deputy.sql
@@ -3,30 +3,119 @@
 -- up for another post-migration script
 CREATE SCHEMA IF NOT EXISTS {pmf_schema};
 
+-- NOTE - HELPER TABLES IN SETUP ARE VALID FOR BOTH MIGRATION PHASES
+-- START OF HELPER TABLES
+
+CREATE TABLE IF NOT EXISTS {pmf_schema}.split_deputies_final (
+    caserecnumber varchar,
+    original_deputy_name varchar,
+    original_deputy_number int,
+    new_deputy_name varchar,
+    organisation_deputy varchar,
+    split_deputy_ecm varchar,
+    add1 varchar,
+    add2 varchar,
+    add3 varchar,
+    city_town varchar,
+    county varchar,
+    postcode varchar,
+    country varchar,
+    tel_day varchar,
+    tel_mobile varchar,
+    tel_evening varchar,
+    email varchar
+);
+\copy {pmf_schema}.split_deputies_final FROM 'IN1201_split_deputies_final.csv' CSV HEADER;
+-- file source: https://opgtransform.atlassian.net/browse/SW-5461
+
+CREATE TABLE IF NOT EXISTS {pmf_schema}.new_deputies (
+    deputy_number int,
+    deputy_name varchar,
+    city_town varchar
+);
+\copy {pmf_schema}.new_deputies FROM 'IN1201_new_deputies.csv' CSV HEADER;
+-- file source: s3://async-upload.casrecdmpq.eu-west-1.sirius.opg.justice.gov.uk/post-migration-files/new_deputies.csv
+
+-- make a mapping table from the two imported documents
+DROP TABLE IF EXISTS {pmf_schema}.changed_deputies;
+SELECT DISTINCT caserecnumber, deputy_number, COALESCE(non_org_deputy_id, org_deputy_id) AS new_deputy_id
+INTO {pmf_schema}.changed_deputies
+FROM (
+    SELECT
+    sd.caserecnumber,
+    sd.original_deputy_number deputy_number,
+    CONCAT(sd.original_deputy_name,' ',sd.city_town),
+    sd.new_deputy_name,
+    nd_non_orgs.deputy_id AS non_org_deputy_id,
+    nd_orgs.deputy_id AS org_deputy_id
+    FROM {pmf_schema}.split_deputies_final sd
+    LEFT JOIN (
+        SELECT DISTINCT
+        replace(CONCAT(p.firstname,' ',p.othernames,' ',p.surname,' ',nd.city_town), '  ', ' ') AS join_name,
+        p.id AS deputy_id
+        FROM {pmf_schema}.new_deputies nd
+        LEFT JOIN {pmf_schema}.deputies deputy_mapping
+            ON deputy_mapping.deputynumber = nd.deputy_number
+        LEFT JOIN persons p
+            ON p.id = deputy_mapping.id
+        WHERE deputy_mapping.clientsource IS NULL
+    ) nd_non_orgs
+        ON nd_non_orgs.join_name = CONCAT(sd.original_deputy_name,' ',sd.city_town)
+    LEFT JOIN (
+        SELECT DISTINCT
+        nd.deputy_name AS join_name,
+        deputy_mapping.id AS deputy_id
+        FROM {pmf_schema}.new_deputies nd
+        LEFT JOIN {pmf_schema}.deputies deputy_mapping
+            ON deputy_mapping.deputynumber = nd.deputy_number
+        WHERE deputy_mapping.clientsource IS NULL
+    ) nd_orgs
+        ON nd_orgs.join_name = sd.new_deputy_name
+) t1;
+
 -- Works for both migrations phases
 DROP TABLE IF EXISTS {pmf_schema}.deputies;
 SELECT id, deputynumber, clientsource
 INTO {pmf_schema}.deputies
 FROM persons
 WHERE type = 'actor_deputy'
-AND (clientsource LIKE 'CASRECMIGRATION%');
+AND (clientsource LIKE 'CASRECMIGRATION%' OR clientsource IS NULL);
 CREATE INDEX idx_pmf1201_deputies_id ON {pmf_schema}.deputies USING btree (id);
 CREATE INDEX idx_pmf1201_deputies_deputynumber ON {pmf_schema}.deputies USING btree (deputynumber);
 
+-- END OF HELPER TABLES
+
 WITH od_updates AS (
     SELECT DISTINCT
-        od.id AS od_id,
-        od.maincorrespondent AS original_maincorrespondent,
-        (CASE WHEN TRIM(ds."Corr") = 'Y' THEN True ELSE False END) AS expected_maincorrespondent
-    FROM {casrec_schema}.deputyship ds
-    INNER JOIN {casrec_mapping}.cases cmc
-        ON od.order_id = cmc.sirius_id
-    INNER JOIN {pmf_schema}.deputies deputy_mapping
-        ON deputy_mapping.deputynumber = CAST(ds."Deputy No" AS INT)
-    LEFT JOIN order_deputy od
-        ON od.order_id = cmc.sirius_id
-        AND od.deputy_id = deputy_mapping.id
-    WHERE deputy_mapping.clientsource = '{client_source}'
+        order_deputy_id,
+        maincorrespondent,
+        expected_maincorrespondent
+    FROM (
+        SELECT DISTINCT
+            ds."Deputy No",
+            ds."Order No",
+            ds."Case",
+            cmc.sirius_id order_id,
+            deputy_mapping.id deputy_id_original,
+            COALESCE(od2.id, od.id) AS order_deputy_id,
+            COALESCE(od2.maincorrespondent, od.maincorrespondent) AS maincorrespondent,
+            (CASE WHEN TRIM(ds."Corr") = 'Y' THEN True ELSE False END) AS expected_maincorrespondent
+        FROM {casrec_schema}.deputyship ds
+        INNER JOIN {casrec_mapping}.cases cmc
+            ON cmc."Order No" = ds."Order No"
+        INNER JOIN {pmf_schema}.deputies deputy_mapping
+            ON deputy_mapping.deputynumber = CAST(ds."Deputy No" AS INT)
+        LEFT JOIN {pmf_schema}.changed_deputies
+            ON changed_deputies.caserecnumber = ds."Case"
+            AND changed_deputies.deputy_number = deputy_mapping.deputynumber
+        LEFT JOIN order_deputy od
+            ON od.order_id = cmc.sirius_id
+            AND od.deputy_id = deputy_mapping.id
+        LEFT JOIN order_deputy od2
+            ON od2.order_id = cmc.sirius_id
+            AND od2.deputy_id = changed_deputies.new_deputy_id
+        WHERE deputy_mapping.clientsource = '{client_source}'
+    ) t1
 )
 SELECT *
 INTO {pmf_schema}.order_deputy_updates
@@ -37,7 +126,7 @@ SELECT od.*
 INTO {pmf_schema}.order_deputy_audit
 FROM order_deputy od
 INNER JOIN {pmf_schema}.order_deputy_updates u
-ON od.id = u.od_id;
+ON od.id = u.order_deputy_id;
 
 --@update_tag
 UPDATE order_deputy od
