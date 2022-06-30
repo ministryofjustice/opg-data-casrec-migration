e3af7c02845042a0f7de1795a0765d46c9014cdf
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-06-30 10:43:26 +0100

    change the start date cutoff

--- migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/24_pmf_reports_in1267/annual_report_logs.sql
+++ migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/24_pmf_reports_in1267/annual_report_logs.sql
@@ -1,39 +1,40 @@
---Purpose: Remove reports on P3 deputies that are prior to first order date as they were entered erroneously
+--Purpose: Remove reports on P3 deputies that are prior to first order date AS they were entered erroneously
 --@setup_tag
 CREATE SCHEMA IF NOT EXISTS {pmf_schema};
 
-WITH earliest_order as
-(select distinct on (p.caserecnumber) p.caserecnumber, c.orderdate
-from persons p
-inner join cases c on c.client_id = p.id
-where p.clientsource = 'CASRECMIGRATION_P3' --Only run for this phase
-and p.type = 'actor_client'
-order by p.caserecnumber, c.orderdate asc)
-select p.caserecnumber, arl.id as arl_id, arld.id as arld_id
-into {pmf_schema}.annual_report_log_deletions
-from annual_report_logs arl
-inner join persons p on p.id = arl.client_id
-inner join earliest_order eo on eo.caserecnumber = p.caserecnumber
-left join annual_report_lodging_details arld ON arl.id = arld.annual_report_log_id
-where arl.reportingperiodenddate::timestamp < eo.orderdate::timestamp;
+WITH earliest_order AS
+(SELECT DISTINCT ON (p.caserecnumber) p.caserecnumber, c.orderdate
+FROM persons p
+INNER JOIN cases c ON c.client_id = p.id
+WHERE p.clientsource = 'CASRECMIGRATION_P3' --Only run for this phase
+AND p.type = 'actor_client'
+order by p.caserecnumber, c.orderdate ASC)
+SELECT p.caserecnumber, arl.id AS arl_id, arld.id AS arld_id
+INTO {pmf_schema}.annual_report_log_deletions
+FROM annual_report_logs arl
+INNER JOIN persons p ON p.id = arl.client_id
+INNER JOIN earliest_order eo ON eo.caserecnumber = p.caserecnumber
+LEFT JOIN annual_report_lodging_details arld ON arl.id = arld.annual_report_log_id
+WHERE (arl.reportingperiodenddate::timestamp < eo.orderdate::timestamp)
+OR (arl.reportingperiodstartdate::timestamp < '2007-10-01 00:00:00'::timestamp);
 
 --@audit_tag
-select arl.*
-into {pmf_schema}.annual_report_logs_audit
-from annual_report_logs arl
-inner join {pmf_schema}.annual_report_log_deletions del on arl.id = del.arl_id;
+SELECT arl.*
+INTO {pmf_schema}.annual_report_logs_audit
+FROM annual_report_logs arl
+INNER JOIN {pmf_schema}.annual_report_log_deletions del ON arl.id = del.arl_id;
 
-select arld.*
-into {pmf_schema}.annual_report_lodging_details_audit
-from annual_report_lodging_details arld
-inner join {pmf_schema}.annual_report_log_deletions del on arld.id = del.arld_id;
+SELECT arld.*
+INTO {pmf_schema}.annual_report_lodging_details_audit
+FROM annual_report_lodging_details arld
+INNER JOIN {pmf_schema}.annual_report_log_deletions del ON arld.id = del.arld_id;
 
 --@update_tag
 DELETE FROM annual_report_lodging_details
-WHERE id IN (select id from {pmf_schema}.annual_report_lodging_details_audit);
+WHERE id IN (SELECT id FROM {pmf_schema}.annual_report_lodging_details_audit);
 
 DELETE FROM annual_report_logs
-WHERE id IN (select id from {pmf_schema}.annual_report_logs_audit);
+WHERE id IN (SELECT id FROM {pmf_schema}.annual_report_logs_audit);
 
 --@validate_tag
 SELECT arl.*
