b1ca120dbc360f445520ed99d10b22995fe6a1d8
jackgoodby <jackgoodby@gmail.com>
2022-07-13 02:52:39 +0100

    Audit tables now hold address data not persons
    Also we want to include caserecnumber to make testing easy, which is easy enough on the pat table but on deputy/deputyships tables requires special efforts to avoid duplication as deputies <-> cases are many to many

--- migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/25_pmf_address_in1272/address.sql
+++ migration_steps/validation/post_migration_fixes/app/pmf_sql_statements/25_pmf_address_in1272/address.sql
@@ -6,16 +6,18 @@ WITH clients_p1 AS (
     SELECT a.id AS address_id, "Case" AS caserecnumber, True AS expected, a.isairmailrequired AS actual
     FROM casrec_csv.pat
     LEFT JOIN persons p
-    ON p.caserecnumber = pat."Case"
+        ON p.caserecnumber = pat."Case"
     LEFT JOIN addresses a
-    ON a.person_id = p.id
+        ON a.person_id = p.id
     WHERE p.type = 'actor_client'
     AND clientsource = 'CASRECMIGRATION'
     AND pat."Foreign" = '1'
+    AND (a.isairmailrequired IS NULL OR a.isairmailrequired != True)
 )
 SELECT *
 INTO jacktest.client_address_updates_p1
 FROM clients_p1;
+-- ~100
 
 -- clients P2
 WITH clients_p2 AS (
@@ -28,10 +30,12 @@ WITH clients_p2 AS (
     WHERE p.type = 'actor_client'
     AND clientsource = 'CASRECMIGRATION_P2'
     AND pat."Foreign" = '1'
+    AND (a.isairmailrequired IS NULL OR a.isairmailrequired != True)
 )
 SELECT *
 INTO jacktest.client_address_updates_p2
 FROM clients_p2;
+-- 0
 
 -- clients P3
 WITH clients_p3 AS (
@@ -44,33 +48,35 @@ WITH clients_p3 AS (
     WHERE p.type = 'actor_client'
     AND clientsource = 'CASRECMIGRATION_P3'
     AND pat."Foreign" = '1'
+    AND (a.isairmailrequired IS NULL OR a.isairmailrequired != True)
 )
 SELECT *
 INTO jacktest.client_address_updates_p3
 FROM clients_p3;
+-- 0
 
 
 --@audit_tag
 -- clients P1
-SELECT p.*
+SELECT a.*
 INTO jacktest.client_address_audit_p1
-FROM persons p
+FROM addresses a
 INNER JOIN jacktest.client_address_updates_p1 cli
-    ON cli.caserecnumber = p.caserecnumber;
+    ON cli.address_id = a.id;
 
 -- clients P2
-SELECT p.*
+SELECT a.*
 INTO jacktest.client_address_audit_p2
-FROM persons p
+FROM addresses a
 INNER JOIN jacktest.client_address_updates_p2 cli
-    ON cli.caserecnumber = p.caserecnumber;
+    ON cli.address_id = a.id;
 
 -- clients P3
-SELECT p.*
+SELECT a.*
 INTO jacktest.client_address_audit_p3
-FROM persons p
+FROM addresses a
 INNER JOIN jacktest.client_address_updates_p3 cli
-    ON cli.caserecnumber = p.caserecnumber;
+    ON cli.address_id = a.id;
 
 
 --@update_tag
@@ -95,31 +101,40 @@ WHERE u.address_id = a.id;
 
 --@validate_tag
 -- clients P1
-SELECT address_id, expected
-FROM jacktest.client_address_updates_p1
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.client_address_audit_p1 aud
-    ON aud.id = a.id;
+SELECT COUNT (1) FROM (
+    SELECT address_id, expected
+    FROM jacktest.client_address_updates_p1
+    EXCEPT
+    SELECT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.client_address_audit_p1 aud
+        ON aud.id = a.id
+) t1;
+-- 100
 
 -- clients P2
-SELECT address_id, expected
-FROM jacktest.client_address_updates_p2
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.client_address_audit_p2 aud
-    ON aud.id = a.id;
+SELECT COUNT (1) FROM (
+    SELECT address_id, expected
+    FROM jacktest.client_address_updates_p2
+    EXCEPT
+    SELECT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.client_address_audit_p2 aud
+        ON aud.id = a.id
+) t1;
+-- 0 ZERO - NO NEED TO UPDATE
 
 -- clients P3
-SELECT address_id, expected
-FROM jacktest.client_address_updates_p3
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.client_address_audit_p3 aud
-    ON aud.id = a.id;
+SELECT COUNT (1) FROM (
+    SELECT address_id, expected
+    FROM jacktest.client_address_updates_p3
+    EXCEPT
+    SELECT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.client_address_audit_p3 aud
+        ON aud.id = a.id
+) t1;
+-- 0 ZERO - NO NEED TO UPDATE
 
 
 -- D E P U T I E S --
@@ -134,92 +149,114 @@ AND clientsource LIKE 'CASRECMIGRATION%';
 CREATE INDEX idx_pmf1272_deputies_id ON jacktest.deputies USING btree (id);
 CREATE INDEX idx_pmf1272_deputies_deputynumber ON jacktest.deputies USING btree (deputynumber);
 
-
 -- deputies P1
 WITH overseas_deputies AS (
-    SELECT DISTINCT
-    d.id,
-    ds."Deputy No"
-    FROM casrec_csv.deputy_address da
-    INNER JOIN casrec_csv.deputyship ds
-        ON ds."Dep Addr No" = da."Dep Addr No"
-    INNER JOIN jacktest.deputies d
-        ON d.deputynumber = CAST(ds."Deputy No" AS INT)
-    WHERE da."Foreign" = '1'
+    SELECT DISTINCT * FROM (
+        SELECT
+            d.id,
+            ds."Case",
+            ds."Deputy No",
+            row_number() over (PARTITION BY ds."Case", ds."Deputy No") AS rownum
+        FROM casrec_csv.deputy_address da
+        INNER JOIN casrec_csv.deputyship ds
+            ON ds."Dep Addr No" = da."Dep Addr No"
+        INNER JOIN jacktest.deputies d
+            ON d.deputynumber = CAST(ds."Deputy No" AS INT)
+            AND d.clientsource = 'CASRECMIGRATION'
+        WHERE da."Foreign" = '1'
+    ) t1 WHERE rownum = 1
 )
 SELECT
     a.id AS address_id,
+    od."Case",
     True AS expected,
     a.isairmailrequired AS actual
 INTO jacktest.deputy_address_updates_p1
 FROM addresses a
 INNER JOIN overseas_deputies od
-    ON od.id = a.person_id;
+    ON od.id = a.person_id
+WHERE (a.isairmailrequired IS NULL OR a.isairmailrequired != True);
+-- 543
 
 -- deputies P2
 WITH overseas_deputies AS (
-    SELECT DISTINCT
-    d.id,
-    ds."Deputy No"
-    FROM casrec_csv_p2.deputy_address da
-    INNER JOIN casrec_csv_p2.deputyship ds
-        ON ds."Dep Addr No" = da."Dep Addr No"
-    INNER JOIN jacktest.deputies d
-        ON d.deputynumber = CAST(ds."Deputy No" AS INT)
-    WHERE da."Foreign" = '1'
+    SELECT DISTINCT * FROM (
+        SELECT
+            d.id,
+            ds."Case",
+            ds."Deputy No",
+            row_number() over (PARTITION BY ds."Case", ds."Deputy No") AS rownum
+        FROM casrec_csv_p2.deputy_address da
+        INNER JOIN casrec_csv_p2.deputyship ds
+            ON ds."Dep Addr No" = da."Dep Addr No"
+        INNER JOIN jacktest.deputies d
+            ON d.deputynumber = CAST(ds."Deputy No" AS INT)
+            AND d.clientsource = 'CASRECMIGRATION_P2'
+        WHERE da."Foreign" = '1'
+    ) t1 WHERE rownum = 1
 )
 SELECT
     a.id AS address_id,
+    od."Case",
     True AS expected,
     a.isairmailrequired AS actual
 INTO jacktest.deputy_address_updates_p2
 FROM addresses a
 INNER JOIN overseas_deputies od
-    ON od.id = a.person_id;
+    ON od.id = a.person_id
+WHERE (a.isairmailrequired IS NULL OR a.isairmailrequired != True);
+-- 0
 
 -- deputies P3
 WITH overseas_deputies AS (
-    SELECT DISTINCT
-    d.id,
-    ds."Deputy No"
-    FROM casrec_csv_p3.deputy_address da
-    INNER JOIN casrec_csv_p3.deputyship ds
-        ON ds."Dep Addr No" = da."Dep Addr No"
-    INNER JOIN jacktest.deputies d
-        ON d.deputynumber = CAST(ds."Deputy No" AS INT)
-    WHERE da."Foreign" = '1'
+    SELECT DISTINCT * FROM (
+        SELECT
+            d.id,
+            ds."Case",
+            ds."Deputy No",
+            row_number() over (PARTITION BY ds."Case", ds."Deputy No") AS rownum
+        FROM casrec_csv_p3.deputy_address da
+        INNER JOIN casrec_csv_p3.deputyship ds
+            ON ds."Dep Addr No" = da."Dep Addr No"
+        INNER JOIN jacktest.deputies d
+            ON d.deputynumber = CAST(ds."Deputy No" AS INT)
+            AND d.clientsource = 'CASRECMIGRATION_P3'
+        WHERE da."Foreign" = '1'
+    ) t1 WHERE rownum = 1
 )
 SELECT
     a.id AS address_id,
+    od."Case",
     True AS expected,
     a.isairmailrequired AS actual
 INTO jacktest.deputy_address_updates_p3
 FROM addresses a
 INNER JOIN overseas_deputies od
-    ON od.id = a.person_id;
-
+    ON od.id = a.person_id
+WHERE (a.isairmailrequired IS NULL OR a.isairmailrequired != True);
+-- 0
 
 --@audit_tag
 -- deputy addresses P1
 SELECT a.*
 INTO jacktest.deputy_address_audit_p1
 FROM addresses a
-INNER JOIN jacktest.deputy_address_updates_p1 du
-    ON du.address_id = a.id;
+INNER JOIN jacktest.deputy_address_updates_p1 dau
+    ON dau.address_id = a.id;
 
 -- deputy addresses P2
 SELECT a.*
 INTO jacktest.deputy_address_audit_p2
 FROM addresses a
-INNER JOIN jacktest.deputy_address_updates_p2 du
-    ON du.address_id = a.id;
+INNER JOIN jacktest.deputy_address_updates_p2 dau
+    ON dau.address_id = a.id;
 
 -- deputy addresses P3
 SELECT a.*
 INTO jacktest.deputy_address_audit_p3
 FROM addresses a
-INNER JOIN jacktest.deputy_address_updates_p3 du
-    ON du.address_id = a.id;
+INNER JOIN jacktest.deputy_address_updates_p3 dau
+    ON dau.address_id = a.id;
 
 
 --@update_tag
@@ -244,28 +281,37 @@ WHERE u.address_id = a.id;
 
 --@validate_tag
 -- deputies P1
-SELECT address_id, expected
-FROM jacktest.deputy_address_updates_p1
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.deputy_address_audit_p1 aud
-    ON aud.id = a.id;
+SELECT COUNT(1) FROM (
+    SELECT DISTINCT address_id, expected
+    FROM jacktest.deputy_address_updates_p1
+    EXCEPT
+    SELECT DISTINCT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.deputy_address_audit_p1 aud
+        ON aud.id = a.id
+) t1;
+-- 540
 
 -- deputies P2
-SELECT address_id, expected
-FROM jacktest.deputy_address_updates_p1
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.deputy_address_audit_p1 aud
-    ON aud.id = a.id;
+SELECT COUNT(1) FROM (
+    SELECT DISTINCT address_id, expected
+    FROM jacktest.deputy_address_updates_p2
+    EXCEPT
+    SELECT DISTINCT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.deputy_address_audit_p2 aud
+        ON aud.id = a.id
+) t1;
+-- 0
 
 -- deputies P3
-SELECT address_id, expected
-FROM jacktest.deputy_address_updates_p1
-EXCEPT
-SELECT id, isairmailrequired
-FROM addresses a
-INNER JOIN jacktest.deputy_address_audit_p1 aud
-    ON aud.id = a.id;
+SELECT COUNT(1) FROM (
+    SELECT address_id, expected
+    FROM jacktest.deputy_address_updates_p3
+    EXCEPT
+    SELECT a.id, a.isairmailrequired
+    FROM addresses a
+    INNER JOIN jacktest.deputy_address_audit_p3 aud
+        ON aud.id = a.id
+) t1;
+-- 0
